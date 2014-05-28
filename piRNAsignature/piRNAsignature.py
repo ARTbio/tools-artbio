4c8d50fbd5966391277455c5f9c39c663f886db5
drosofff <drosofff@gmail.com>
2014-05-28 18:41:10 +0200

    piRNAsignature.xml tool now wrapps both merged signatures and separate signatures displayed in lattice. An r_wrapper.sh version has been put in the piRNAsignature directory rm ../lattice_signature.py ../Lattice_signatures.xml from the GEDtools root directory

--- piRNAsignature/piRNAsignature.py
+++ piRNAsignature/piRNAsignature.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # script for computing overlap signatures from a bowtie output
 # Christophe Antoniewski <drosofff@gmail.com>
-# Usage piRNAsignature.py <1:input> <2:format of input> <3:minsize query> <4:maxsize query> <5:minsize target> <6:maxsize target> <7:minscope> <8:maxscope> <9:output> <10:bowtie index> <11:procedure option>
+# Usage piRNAsignature.py <1:input> <2:format of input> <3:minsize query> <4:maxsize query> <5:minsize target> <6:maxsize target>
+#			  <7:minscope> <8:maxscope> <9:output> <10:bowtie index> <11:procedure option> <12: graph (global or lattice)>
 
 import sys, subprocess
 from smRtools import *
@@ -34,39 +35,39 @@ minscope = int(sys.argv[7])
 maxscope = int(sys.argv[8]) + 1
 general_frequency_table = dict ([(i,0) for i in range(minscope,maxscope)])
 general_percent_table = dict ([(i,0) for i in range(minscope,maxscope)])
-
-
-###### for normalized summing of local_percent_table(s)
-readcount_dic = {}
-Total_read_in_objDic = 0
-for item in objDic:
-  readcount_dic[item] = objDic[item].readcount(minquery, maxquery)
-  Total_read_in_objDic += readcount_dic[item]
-######
-
-
 OUT = open (sys.argv[9], "w")
-for x in (objDic):
-  local_frequency_table = objDic[x].signature( minquery, maxquery, mintarget, maxtarget, range(minscope,maxscope) )
-  local_percent_table = objDic[x].hannon_signature( minquery, maxquery, mintarget, maxtarget, range(minscope,maxscope) )
-  try:
-    for overlap in local_frequency_table.keys():
-      general_frequency_table[overlap] = general_frequency_table.get(overlap, 0) + local_frequency_table[overlap]
-  except:
-    pass
-  try:
-    for overlap in local_percent_table.keys():
-      general_percent_table[overlap] = general_percent_table.get(overlap, 0) + (1./Total_read_in_objDic*readcount_dic[x]*local_percent_table[overlap])
-  except:
-    pass
 
-print >> OUT, "overlap\tnum of pairs\tprobability"
+if sys.argv[12] == "global":
+  ###### for normalized summing of local_percent_table(s)
+  readcount_dic = {}
+  Total_read_in_objDic = 0
+  for item in objDic:
+    readcount_dic[item] = objDic[item].readcount(minquery, maxquery)
+    Total_read_in_objDic += readcount_dic[item]
+  ######
+  for x in (objDic):
+    local_frequency_table = objDic[x].signature( minquery, maxquery, mintarget, maxtarget, range(minscope,maxscope) )
+    local_percent_table = objDic[x].hannon_signature( minquery, maxquery, mintarget, maxtarget, range(minscope,maxscope) )
+    try:
+      for overlap in local_frequency_table.keys():
+        general_frequency_table[overlap] = general_frequency_table.get(overlap, 0) + local_frequency_table[overlap]
+    except:
+      pass
+    try:
+      for overlap in local_percent_table.keys():
+        general_percent_table[overlap] = general_percent_table.get(overlap, 0) + (1./Total_read_in_objDic*readcount_dic[x]*local_percent_table[overlap])
+    except:
+      pass
+  print >> OUT, "overlap\tnum of pairs\tprobability"
+  for classe in sorted(general_frequency_table):
+    print >> OUT, "%i\t%i\t%f" % (classe, general_frequency_table[classe], general_percent_table[classe])
 
-# if minquery==mintarget and maxquery==maxtarget: # the division / 2 decision is now directly taken in the method of smRtools (26/11/2013)
-for classe in sorted(general_frequency_table):
-  print >> OUT, "%i\t%i\t%f" % (classe, general_frequency_table[classe], general_percent_table[classe])
-#else:
-#  for classe in sorted(general_frequency_table):
-#    print >> OUT, "%i\t%i\t%f" % (classe, general_frequency_table[classe], general_percent_table[classe])
+else:
+  print >> OUT, "overlap\tnum of pairs\tprobability\titem"
+  for x in (objDic):
+    local_frequency_table = objDic[x].signature( minquery, maxquery, mintarget, maxtarget, range(minscope,maxscope) )
+    local_percent_table = objDic[x].hannon_signature( minquery, maxquery, mintarget, maxtarget, range(minscope,maxscope) )
+    for classe in range(minscope,maxscope):
+      print >> OUT, "%i\t%i\t%f\t%s" % (classe, local_frequency_table[classe], local_percent_table[classe], x)
 
 OUT.close()
