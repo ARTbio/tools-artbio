0f8547e47dc5abdf504e76c67dbdba3711c5479f
drosofff <drosofff@gmail.com>
2014-10-20 13:17:40 +0200

    moving piRNAsignature tool to argparse mode. Consider changing the name of the tool from piRNA signature to smRNAsignature or just signature

--- piRNAsignature/piRNAsignature.py
+++ piRNAsignature/piRNAsignature.py
@@ -3,52 +3,66 @@
 # Christophe Antoniewski <drosofff@gmail.com>
 # Usage piRNAsignature.py <1:input> <2:format of input> <3:minsize query> <4:maxsize query> <5:minsize target> <6:maxsize target>
 #			  <7:minscope> <8:maxscope> <9:output> <10:bowtie index> <11:procedure option> <12: graph (global or lattice)>
-#			  <13: R code>
+# 			  <13: R code>
+# version 1.1 : argparsing
 
-import sys, subprocess
+import sys, subprocess, argparse
 from smRtools import *
 from collections import defaultdict # test whether it is required
 
-if sys.argv[11] == "--extract_index":
-  if sys.argv[2] == "tabular":
-    Genome = HandleSmRNAwindows (sys.argv[1],"tabular",sys.argv[10],"bowtieIndex")
-  elif sys.argv[2] == "sam":
-    Genome = HandleSmRNAwindows (sys.argv[1],"sam",sys.argv[10],"bowtieIndex")
-  else:
-    Genome = HandleSmRNAwindows (sys.argv[1],"bam",sys.argv[10],"bowtieIndex")
+def Parser():
+  the_parser = argparse.ArgumentParser()
+  the_parser.add_argument('--input', action="store", type=str, help="input alignment file")
+  the_parser.add_argument('--inputFormat', action="store", type=str, choices=["tabular", "bam", "sam"], help="format of alignment file (tabular/bam/sam)")
+  the_parser.add_argument('--minquery', type=int, help="Minimum readsize of query reads (nt) - must be an integer")
+  the_parser.add_argument('--maxquery', type=int, help="Maximum readsize of query reads (nt) - must be an integer")
+  the_parser.add_argument('--mintarget', type=int, help="Minimum readsize of target reads (nt) - must be an integer")
+  the_parser.add_argument('--maxtarget', type=int, help="Maximum readsize of target reads (nt) - must be an integer")
+  the_parser.add_argument('--minscope', type=int, help="Minimum overlap analyzed (nt) - must be an integer")
+  the_parser.add_argument('--maxscope', type=int, help="Maximum overlap analyzed (nt) - must be an integer")
+  the_parser.add_argument('--outputOverlapDataframe', action="store", type=str, help="Overlap dataframe")
+  the_parser.add_argument('--referenceGenome', action='store', help="path to the bowtie-indexed or fasta reference")
+  the_parser.add_argument('--extract_index', action='store_true', help="specify if the reference is an indexed Bowtie reference")
+  the_parser.add_argument('--graph', action='store', choices=["global", "lattice"], help="small RNA signature is computed either globally or by item (global-lattice)")
+  the_parser.add_argument('--rcode', type=str, help="R code to be passed to the python script")
+  args = the_parser.parse_args()
+  return args
+
+args = Parser()
+
+
+if args.extract_index:
+  GenomeFormat = "bowtieIndex"
 else:
-  if sys.argv[2] == "tabular":
-    Genome = HandleSmRNAwindows (sys.argv[1],"tabular",sys.argv[10],"fastaSource") 
-  elif sys.argv[2] == "sam":
-    Genome = HandleSmRNAwindows (sys.argv[1],"sam",sys.argv[10],"fastaSource")
-  else:
-    Genome = HandleSmRNAwindows (sys.argv[1],"bam",sys.argv[10],"fastaSource")
-# this decisional tree may be simplified if sam and bam inputs are treated the same way by pysam
+  GenomeFormat = "fastaSource"
+
+if args.inputFormat == "tabular":
+  Genome = HandleSmRNAwindows (args.input, args.inputFormat, args.referenceGenome, GenomeFormat)
+elif args.inputFormat == "sam":
+  Genome = HandleSmRNAwindows (args.input, args.inputFormat, args.referenceGenome, GenomeFormat)
+else:
+  Genome = HandleSmRNAwindows (args.input, args.inputFormat, args.referenceGenome, GenomeFormat)
 
 # replace objDic by Genome.instanceDict or... objDic = Genome.instanceDict
 objDic = Genome.instanceDict
 
-minquery = int(sys.argv[3])
-maxquery = int(sys.argv[4])
-mintarget = int(sys.argv[5])
-maxtarget = int(sys.argv[6])
-minscope = int(sys.argv[7])
-maxscope = int(sys.argv[8]) + 1
-general_frequency_table = dict ([(i,0) for i in range(minscope,maxscope)])
-general_percent_table = dict ([(i,0) for i in range(minscope,maxscope)])
-OUT = open (sys.argv[9], "w")
+args.maxscope += 1
 
-if sys.argv[12] == "global":
+general_frequency_table = dict ([(i,0) for i in range(args.minscope, args.maxscope)])
+general_percent_table = dict ([(i,0) for i in range(args.minscope, args.maxscope)])
+OUT = open (args.outputOverlapDataframe, "w")
+
+if args.graph == "global":
   ###### for normalized summing of local_percent_table(s)
   readcount_dic = {}
   Total_read_in_objDic = 0
   for item in objDic:
-    readcount_dic[item] = objDic[item].readcount(minquery, maxquery)
+    readcount_dic[item] = objDic[item].readcount(args.minquery, args.maxquery)
     Total_read_in_objDic += readcount_dic[item]
   ######
   for x in (objDic):
-    local_frequency_table = objDic[x].signature( minquery, maxquery, mintarget, maxtarget, range(minscope,maxscope) )
-    local_percent_table = objDic[x].hannon_signature( minquery, maxquery, mintarget, maxtarget, range(minscope,maxscope) )
+    local_frequency_table = objDic[x].signature( args.minquery, args.maxquery, args.mintarget, args.maxtarget, range(args.minscope, args.maxscope) )
+    local_percent_table = objDic[x].hannon_signature( args.minquery, args.maxquery, args.mintarget, args.maxtarget, range(args.minscope, args.maxscope) )
     try:
       for overlap in local_frequency_table.keys():
         general_frequency_table[overlap] = general_frequency_table.get(overlap, 0) + local_frequency_table[overlap]
@@ -66,13 +80,13 @@ if sys.argv[12] == "global":
 else:
   print >> OUT, "overlap\tnum of pairs\tprobability\titem"
   for x in (objDic):
-    local_frequency_table = objDic[x].signature( minquery, maxquery, mintarget, maxtarget, range(minscope,maxscope) )
-    local_percent_table = objDic[x].hannon_signature( minquery, maxquery, mintarget, maxtarget, range(minscope,maxscope) )
-    for classe in range(minscope,maxscope):
+    local_frequency_table = objDic[x].signature( args.minquery, args.maxquery, args.mintarget, args.maxtarget, range(args.minscope, args.maxscope) )
+    local_percent_table = objDic[x].hannon_signature( args.minquery, args.maxquery, args.mintarget, args.maxtarget, range(args.minscope, args.maxscope) )
+    for classe in range(args.minscope, args.maxscope):
       print >> OUT, "%i\t%i\t%f\t%s" % (classe, local_frequency_table[classe], local_percent_table[classe], x)
 
 OUT.close()
 
 ## Run the R script that is defined in the xml using the Rscript binary provided with R.
-R_command="Rscript "+ sys.argv[13]
+R_command="Rscript "+ args.rcode
 process = subprocess.Popen(R_command.split())
