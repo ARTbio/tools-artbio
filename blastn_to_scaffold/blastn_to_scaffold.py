2d593b51f5e1b9941f54d1782aedcb45dd3b0f09
drosofff <drosofff@gmail.com>
2015-06-11 18:27:38 +0200

    further implementing blastn_to_scaffold for Marius

--- blastn_to_scaffold/blastn_to_scaffold.py
+++ blastn_to_scaffold/blastn_to_scaffold.py
@@ -8,11 +8,24 @@ def insert_newlines(string, every=60):
         lines.append(string[i:i+every])
     return '\n'.join(lines)
     
+def getseq (fastadict, transcript, up, down, orientation="direct"):
+    def reverse (seq):
+        revdict = {"A":"T","T":"A","G":"C","C":"G","N":"N"}
+        revseq = [revdict[i] for i in seq[::-1]]
+        return "".join(revseq)
+    pickseq = fastadict[transcript][up-1:down]
+    if orientation == "direct":
+        return pickseq
+    else:
+        return reverse(pickseq)
+
 def Parser():
     the_parser = argparse.ArgumentParser(
         description="Generate DNA scaffold from blastn alignment of Contigs")
     the_parser.add_argument(
         '--sequences', action="store", type=str, help="input sequence file in fasta format")
+    the_parser.add_argument(
+        '--guideSequence', action="store", type=str, help="the reference sequence to guide the scaffold assembly in fasta format")
     the_parser.add_argument(
         '--blastn-tab', dest="blastn_tab", action="store", type=str, help="13-columns tabular blastn output")
     the_parser.add_argument(
@@ -26,6 +39,7 @@ def __main__():
     BlastnOutput = open (args.blastn_tab, "r")
     Contigs = open (args.sequences, "r")
     ContigsDict = {}
+    GuideDict = {}
     protScaffold = {}
     
     for line in Contigs:
@@ -35,7 +49,19 @@ def __main__():
         else:
             ContigsDict[header] += line[:-1]
             
-    dnaScaffold = dict ( [(i,"N") for i in range (1, dnaLenght+1)] )
+    dnaScaffold = {}
+    
+    Guide = open(args.guideSequence, "r")
+    coordinate = 0
+    for line in Guide:
+        if line[0] == ">":
+            continue
+        else:
+            for nucleotide in line[:-1]:
+                coordinate += 1
+                GuideDict[coordinate] = nucleotide.lower()
+    Guide.close()
+    
         
     for line in BlastnOutput:
         fields = line[:-1].split("\t")
@@ -43,15 +69,22 @@ def __main__():
         queryStop = int(fields[7])
         subjectStart = int(fields[8])
         subjectStop = int(fields[9])
+        if subjectStart > subjectStop:
+            subjectStart, subjectStop = subjectStop, subjectStart
+            orientation = "reverse"
+        else:
+            orientation = "direct"
         seqHeader = fields[0]
-        sequence = ContigsDict[seqHeader]
-        for i in range (subjectStart, subjectStop):
-            del dnaScaffold[i]
-        dnaScaffold[subjectStop] = ContigsDict[seqHeader][queryStart -1: queryStop]
+        sequence = getseq (ContigsDict, seqHeader, queryStart, queryStop, orientation)
+        for i, nucleotide in enumerate(sequence):
+            dnaScaffold[i+subjectStart] = nucleotide
         
     finalSeqList = []
-    for i in sorted (dnaScaffold):
-        finalSeqList.append(dnaScaffold[i])
+    for i in range(1, dnaLenght+1) :
+        try:
+            finalSeqList.append(dnaScaffold[i])
+        except KeyError:
+            finalSeqList.append(GuideDict[i])
     finalSequence = insert_newlines("".join(finalSeqList))
      
     Out = open (args.output, "w")
