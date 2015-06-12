a1eb59c3ef3d987cf32d2be8887afa4c5d75ec5e
drosofff <drosofff@gmail.com>
2015-06-12 17:58:00 +0200

    New blastn_to_scaffold code, base on contig insertions from the end to the start of the reference guide

--- blastn_to_scaffold/blastn_to_scaffold.py
+++ blastn_to_scaffold/blastn_to_scaffold.py
@@ -32,68 +32,86 @@ def Parser():
         '--output', action="store", type=str, help="output file path, fasta format")
     args = the_parser.parse_args()
     return args
-
-def __main__():
-    args = Parser()
-    dnaLenght = int (open (args.blastn_tab, "r").readline().split("\t")[12])
-    BlastnOutput = open (args.blastn_tab, "r")
-    Contigs = open (args.sequences, "r")
-    ContigsDict = {}
-    GuideDict = {}
-    protScaffold = {}
     
-    for line in Contigs:
-        if line[0] == ">":
-            header = line[1:-1]
-            ContigsDict[header] = ""
-        else:
-            ContigsDict[header] += line[:-1]
-            
-    dnaScaffold = {}
+def blatnInfo (file):
+    blastlist = []
+    with open(file, "r") as f:
+        for line in f:
+            minilist = []
+            fields = line.rstrip().split()
+            minilist.append(fields[0])
+            minilist.extend(fields[6:10])
+            blastlist.append(minilist)
+    blastlist.sort(key=lambda x: x[3], reverse=True)
+    return blastlist
     
-    Guide = open(args.guideSequence, "r")
+def myContigs (file):
+    Contigs = {}
+    with open(file, "r") as f:
+        for line in f:
+            if line[0] == ">":
+                header = line[1:-1]
+                Contigs[header] = ""
+            else:
+                Contigs[header] += line[:-1]
+    return Contigs
+    
+def myGuide (file):
+    Guide = {}
     coordinate = 0
-    for line in Guide:
-        if line[0] == ">":
-            continue
-        else:
-            for nucleotide in line[:-1]:
-                coordinate += 1
-                GuideDict[coordinate] = nucleotide.lower()
-    Guide.close()
-    
-        
-    for line in BlastnOutput:
-        fields = line[:-1].split("\t")
-        queryStart = int(fields[6])
-        queryStop = int(fields[7])
-        subjectStart = int(fields[8])
-        subjectStop = int(fields[9])
+    with open(file, "r") as f:
+        for line in f:
+            if line[0] == ">":
+                continue
+            else:
+                for nucleotide in line[:-1]:
+                    coordinate += 1
+                    Guide[coordinate] = nucleotide.lower()
+    return Guide
+
+def updateGuide (blastlist, GuideDict, ContigsDict):
+    '''
+    the blastlist object is a list of list with
+    element [0] : name of the blasted Contig
+    element [1] : queryStart of the alignment to the reference
+    element [2] = queryStop of the alignment to the reference        
+    element [3] : subjectStart of the alignment to the reference
+    element [4] = subjectStop of the alignment to the reference        
+    '''
+    for fields in blastlist:
+        seqHeader = fields[0]
+        queryStart = int(fields[1])
+        queryStop = int(fields[2])
+        subjectStart = int(fields[3])
+        subjectStop = int(fields[4])
         if subjectStart > subjectStop:
             subjectStart, subjectStop = subjectStop, subjectStart
             orientation = "reverse"
         else:
             orientation = "direct"
-        seqHeader = fields[0]
         sequence = getseq (ContigsDict, seqHeader, queryStart, queryStop, orientation)
+        for i in range(subjectStart, subjectStop+1):
+            del GuideDict[i]
         for i, nucleotide in enumerate(sequence):
-            dnaScaffold[i+subjectStart] = nucleotide
-        
+            GuideDict[i+subjectStart] = nucleotide
+            
+def finalAssembly (GuideDict, outputfile):
     finalSeqList = []
-    for i in range(1, dnaLenght+1) :
-        try:
-            finalSeqList.append(dnaScaffold[i])
-        except KeyError:
-            finalSeqList.append(GuideDict[i])
-    finalSequence = insert_newlines("".join(finalSeqList))
-     
-    Out = open (args.output, "w")
+    for keys in sorted(GuideDict):
+        finalSeqList.append(GuideDict[keys])
+    finalSequence = insert_newlines("".join(finalSeqList) )
+    Out = open (outputfile, "w")
     print >> Out, ">Scaffold"
     print >> Out, finalSequence
-            
-    BlastnOutput.close()
-    Contigs.close()
     Out.close()
-        
+    
+def __main__():
+    args = Parser()
+    ContigsDict = myContigs (args.sequences)
+    GuideDict = myGuide (args.guideSequence)
+    blastlist = blatnInfo(args.blastn_tab)
+    updateGuide(blastlist, GuideDict, ContigsDict)
+    finalAssembly(GuideDict, args.output)
+
 if __name__ == "__main__":
     __main__()
