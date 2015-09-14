b527274cd2fa83bb28c7f8fd001c612faeffff33
Fabio Rocha Jimenez Vieira <fabiorjvieira@gmail.com>
2015-09-14 18:26:12 +0200

    File qrCoder.tar was not necessary, so it was removed.
    Update file structure and contents to match ARTbio development protocol
    
    Error on tool xml file test tag fixed
    
    nother tool xml test tag malformation
    
    ok
    
    qrCode directory changed to qr_code to match galaxy repository name rule
    
    ok
    
    Building dependencies for qr_coder.
    
    Testing with planemo --shed_target option
    
    Testing with planemo --shed_target option
    
    .shed.xml fixed. Now pointing to "https://lbcd41.snv.jussieu.fr/toolshed/" and "testtoolshed"
    
    Fixed the download version of packages image and qrcode and the site url of image.
    
    Make qrFit precedes qrversion in qr_coder
    
    ok
    
    ok
    
    Fixed tool dependencies xml files.
    
    Image -> image (galaxy packages must be in lowercase).
    
    Fixed qrFit problem on python code.
    
    Now qr_coder instalation dependes on image and qrcode instalations (see qr_coder tool_dependencies.xml)
    
    Dependecies fixed. No need for packages anymore.
    
    Packages image and qrcode removed (no more needed for qr_coder tool).
    
    Useless comments removed.

--- tools/qr_coder/qr_coder.py
+++ tools/qr_coder/qr_coder.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
-import qrcode, argparse, sys
+import qrcode
+import argparse
+import sys
 
 class Constants:
-   PARAMETER_DELIMITATOR = "[]"
+   PARAMETER_DELIMITER = "[]"
    VERSION = "v1.0"
    IMAGE_TYPE = '.png'
    IMAGE_DEFAULT = 'demo'
@@ -14,14 +16,10 @@ class Constants:
    HIGH = 'High'
    PARAMETERS = "#Parameters passed by user"
 
-def qrCoder(qrData, qrVersion, qrErrorCorrection, qrBoxSize, qrBorder, qrFit, qrOutput):
+def qr_coder(qrData, qrVersion, qrErrorCorrection, qrBoxSize, qrBorder, qrOutput):
    if qrData is "":
          print >> sys.stderr, Constants.ERROR_QR_DATA
          sys.exit(-1)
-   if qrVersion is "":
-      qrVersion = 1
-
-   print qrErrorCorrection, Constants.LOW
 
    if qrErrorCorrection is "":
       qrErrorCorrection = qrcode.constants.ERROR_CORRECT_L
@@ -34,27 +32,41 @@ def qrCoder(qrData, qrVersion, qrErrorCorrection, qrBoxSize, qrBorder, qrFit, qr
    elif qrErrorCorrection is Constants.HIGH:
       qrErrorCorrection = qrcode.constants.ERROR_CORRECT_H
    
-   print qrErrorCorrection, Constants.LOW
    qrErrorCorrection = qrcode.constants.ERROR_CORRECT_L
 
    if qrBoxSize is "":
       qrBoxSize = 10
    if qrBorder is "":
       qrBorder = 4
-   if qrFit is "":
-      qrFit = False
+
+   if qrVersion is "":
+      qrVersion = 0
+   qrVersion=int(qrVersion)
+   if qrVersion < 1:
+      qrVersion = 1
+   elif qrVersion > 40:
+      qrVersion = 40
+
    if qrOutput is "":
       qrOutput = Constants.IMAGE_DEFAULT + Constants.IMAGE_TYPE
       
-   qr = qrcode.QRCode(
-       version=qrVersion,
-       error_correction=qrErrorCorrection,
-       box_size=qrBoxSize,
-       border=qrBorder
-   )
+   if qrVersion == 0:
+      qr = qrcode.QRCode(
+         error_correction=qrErrorCorrection,
+         box_size=qrBoxSize,
+         border=qrBorder
+         )
+   else:
+      qr = qrcode.QRCode(
+         version=qrVersion,
+         error_correction=qrErrorCorrection,
+         box_size=qrBoxSize,
+         border=qrBorder
+         )
+
    qr.add_data(qrData)
-   if qrFit:
-      qr.make(fit=qrFit)
+   if qrVersion == 0:
+      qr.make(fit=True)
    else:
       qr.make()
    image = qr.make_image()
@@ -64,22 +76,22 @@ def printParameters(argumentList):
    print Constants.PARAMETERS
    parameters = vars(argumentList)
    for parameter in parameters:
-      print parameter, Constants.PARAMETER_DELIMITATOR[0] + str(parameters[parameter]) + Constants.PARAMETER_DELIMITATOR[1]
+      print parameter, Constants.PARAMETER_DELIMITER[0] + str(parameters[parameter]) + Constants.PARAMETER_DELIMITER[1]
    print ""
    
 def main():
    parser = argparse.ArgumentParser(prog = "qr Coder", usage = "%(prog)s -qrData {data} [options]", description="QR Code image generator. " + Constants.VERSION)
    parser.add_argument("-qrData",            dest="qrData",            default="", help="Data to be coded as an qr image",                                   required=True)
    parser.add_argument("-qrOutput",          dest="qrOutput",          default="", help="Name of the image PNG file",                                        required=False)
-   parser.add_argument("-qrVersion",         dest="qrVersion",         default="", help="An integer from 1 to 40 that controls the size of the QR Code",     required=False)
-   parser.add_argument("--qrFit",            dest="qrFit",             default="", help="Makes the code to determine the size of the QR Code automatically", required=False, action='store_true')
+   parser.add_argument("-qrVersion",         dest="qrVersion",         default="", help="An integer from 0 (automatic) to 40 that controls the size of the QR Code",     required=False)
+   #parser.add_argument("--qrFit",            dest="qrFit",             default="", help="Makes the code to determine the size of the QR Code automatically", required=False, action='store_true')
    parser.add_argument("-qrBorder",          dest="qrBorder",          default="", help="Controls how many boxes thick the border should be",                required=False)
    parser.add_argument("-qrBoxSize",         dest="qrBoxSize",         default="", help="Controls how many pixels each 'box' of the QR code is",             required=False)
    parser.add_argument("-qrErrorCorrection", dest="qrErrorCorrection", default="", help="Controls the error correction used for the QR Code",                required=False, choices=[Constants.LOW, Constants.MEDIUM, Constants.Q, Constants.HIGH])
 
    argumentList = parser.parse_args()
    printParameters(argumentList)
-   qrCoder(argumentList.qrData, argumentList.qrVersion, argumentList.qrErrorCorrection, argumentList.qrBoxSize, argumentList.qrBorder, argumentList.qrFit, argumentList.qrOutput)
+   qr_coder(argumentList.qrData, argumentList.qrVersion, argumentList.qrErrorCorrection, argumentList.qrBoxSize, argumentList.qrBorder, argumentList.qrOutput)
    
 #real main stream
 if __name__ == "__main__":
