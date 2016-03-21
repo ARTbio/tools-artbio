0651eb8c86d890e4b223fec82ab3980932710030
drosofff <drosofff@gmail.com>
2016-03-21 22:29:36 +0100

    fix bugs in sam_to_fastq py and xml files

--- tools/sam_to_fastq/sam_to_fastq.py
+++ tools/sam_to_fastq/sam_to_fastq.py
@@ -15,18 +15,18 @@ def Parser():
         
 def print_fastq_sequence(samline, file):
   samfields = samline[:-1].split("\t")
-  print >> file, '%s\n%s\n%s\n%s' % (samfields[0], samfields[9], samfields[0], samfields[10])
+  file.write ( '@%s\n%s\n+\n%s' % (samfields[0], samfields[9], samfields[10]) )
 
 def main(input, output):
     infile = open (input, "r")
     outfile = open (output, "w")
-    for line in infile:
-        if line[0] == "@":
-            continue
-        if line.split("\t") != "4":
-            print_fastq_sequence (line, outfile)
-    infile.close()
-    outfile.close()
+    with open (input, "r") as infile:
+        with open (output, "w") as outfile:
+            for line in infile:
+                if line[0] == "@":
+                    continue
+                if line.split("\t")[1] != "4":
+                    print_fastq_sequence (line, outfile)
 
 if __name__ == "__main__":
     args = Parser()
