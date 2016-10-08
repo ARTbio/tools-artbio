b916b6a840aee51d323d804ed213444469299bc8
Marius van den Beek <m.vandenbeek@gmail.com>
2016-10-08 22:13:18 +0200

    Lint sam_to_fastq, skip deprecatd interpreter flag
    and add to testing

--- tools/sam_to_fastq/sam_to_fastq.py
+++ tools/sam_to_fastq/sam_to_fastq.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #
-import sys
 import argparse
+
 
 def Parser():
     the_parser = argparse.ArgumentParser()
@@ -11,23 +11,23 @@ def Parser():
         '--output', action="store", type=str, help="output FASTQ file")
     args = the_parser.parse_args()
     return args
-    
-        
-def print_fastq_sequence(samline, file):
-  samfields = samline[:-1].split("\t")
-  file.write ( '@%s\n%s\n+\n%s\n' % (samfields[0], samfields[9], samfields[10]) )
 
-def main(input, output):
-    infile = open (input, "r")
-    outfile = open (output, "w")
-    with open (input, "r") as infile:
-        with open (output, "w") as outfile:
+
+def print_fastq_sequence(samline, file_obj):
+    samfields = samline[:-1].split("\t")
+    file_obj.write('@%s\n%s\n+\n%s\n' % (samfields[0], samfields[9], samfields[10]))
+
+
+def main(infile, output):
+    with open(infile, "r") as infile:
+        with open(output, "w") as outfile:
             for line in infile:
                 if line[0] == "@":
                     continue
                 if line.split("\t")[1] != "4":
-                    print_fastq_sequence (line, outfile)
+                    print_fastq_sequence(line, outfile)
+
 
 if __name__ == "__main__":
     args = Parser()
-    main (args.input, args.output)
+    main(args.input, args.output)
