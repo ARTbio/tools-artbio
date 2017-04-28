4a1e5a180172371a983c62745b1dcbd9adbb64fd
Chaimae EL JAOUHARI <c.eljaouhari@gmail.com>
2017-04-28 19:24:45 +0200

    adding dict reference(name:length) and dict Reference(name: pos,nbr_reads, polarity)

--- tools/small_rna_map/small_rna_map.py
+++ tools/small_rna_map/small_rna_map.py
@@ -13,18 +13,44 @@ def Parser():
 bamfile= pysam.AlignmentFile("/home/artbio/Bureau/input.bam", "rb")
 output = open("/home/artbio/Bureau/output2.tab", "w")
 
-Ref = bamfile.references #Reference names
-lengths = bamfile.lengths # Reference lengths
-nbr_read=0
+def Ref_length(bamfile):
+	Ref = bamfile.references #Reference names
+	lengths = bamfile.lengths # Reference lengths
+	filename = bamfile.filename # filename
+	ref_length = {}
+	j=0
+	for ref in Ref:
+		ref_length[ref]=lengths[j]
+		j += 1
+	return ref_length		
 
-for ref in Ref:
+
+
+def Ref_coord_pol(bamfile):
+	ref_coord ={}
+	i=0
 	for read in bamfile:
+		polarity = "Forward" if (not read.is_reverse) else "Reverse"
 		if not read.is_unmapped:
-			if ref==bamfile.getrname(read.reference_id):
-				nbr_read=1+nbr_read
-	
+			read_rname = read.reference_name
+			read_position = read.pos
 			
-	output.write(ref+"\t"+str(read.rlen)+"\t"+str(nbr_read)+"\n")
+			if ref_coord.has_key(read_rname): 
+				
+				if int(ref_coord[read_rname][i])==read_position:
+					ref_coord[read_rname][i] += 1 
+				else : 
+					ref_coord[read_rname].append(1)
+					i = i+1				
+			else :	
+				ref_coord[read_rname] = [1]
+				i=0			
+		continue
+		ref_coord[read_rname].append(polarity)
+	return ref_coord
+
+
+
 			
 output.close()
 bamfile.close()
