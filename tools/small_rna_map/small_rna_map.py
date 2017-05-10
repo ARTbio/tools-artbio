b9425d66136cebcb2856a82258ac18980be370eb
chamaelj <c.eljaouhari@gmail.com>
2017-05-10 18:55:20 +0200

    Plotting with ggplot2 package

--- tools/small_rna_map/small_rna_map.py
+++ tools/small_rna_map/small_rna_map.py
@@ -35,19 +35,20 @@ def Ref_coord_pol(bamfile):
                         read_rname = read.reference_name
                         read_position = read.pos + 1
                         the_key = (read_rname, read_position,polarity)
+                        cigar = read.cigar[0][1]
                         try:
-                                ref_coord[the_key] = ref_coord[the_key] + 1
-                                
+                                ref_coord[the_key][0] = ref_coord[the_key][0]+1
+                                ref_coord[the_key][1].append(cigar)
                         except:
-                                ref_coord[the_key] = 1
-        return collections.OrderedDict(sorted(ref_coord.items())) #dictionary {(read_rname,read_position,polarity):nmbr_read}
+                                ref_coord[the_key] = [1,[cigar]]
+        return collections.OrderedDict(sorted(ref_coord.items())) #dictionary {(read_rname,read_position,polarity):[nmbr_read,list_of_alignment_length]}
 
 
 def calcul_stat(dictionary,ref):
 	dico = {}	
 	for keys in dictionary:
 		key = keys[0] #reference name
-		value = keys[1] # position
+		value = dictionary[keys][0] # position
 		try:
 			dico[key].append(value)
 		except: 
@@ -55,8 +56,7 @@ def calcul_stat(dictionary,ref):
 			dico[key].append(value)
 	the_max = max(dico[ref])
 	the_mean = numpy.mean(dico[ref])
-	the_median = numpy.median(dico[ref])
-	return (the_max, round(the_mean,2), the_median)
+	return (the_max, round(the_mean,2))
 
 
 def filename(infile): 
@@ -71,8 +71,10 @@ def main(infile, output):
                         outfile.write("Dataset" + "\t" + "Chromosome"+ "\t" + "Chrom_length" + "\t" + "Coordinate" + "\t" + "Nbr_reads" + "\t" + "Polarity" + "\t" + "Max" + "\t" + "Mean" + "\t" + "Median" + "\n")
                         ref_coord = Ref_coord_pol(infile)
                         ref_length = Ref_length(infile)
+                        tabulation = "\t"
                         for read in ref_coord:
-				outfile.write(str(filename(infile)) + "\t" + str(read[0]) + "\t" + str(ref_length[str(read[0])]) + "\t" + str(read[1]) + "\t" + str(ref_coord[read])+ "\t" + str(read[2]) + "\t" + str(calcul_stat(ref_coord,read[0])[0]) + "\t" + str(calcul_stat(ref_coord,read[0])[1]) + "\t" + str(calcul_stat(ref_coord,read[0])[2])+ "\n")
+				table = (str(filename(infile)), str(read[0]), str(ref_length[str(read[0])]), str(read[1]), str(ref_coord[read][0]), str(read[2]), str(calcul_stat(ref_coord,read[0])[0]), str(calcul_stat(ref_coord,read[0])[1]), str(numpy.median(ref_coord[read][1])))
+				outfile.write(tabulation.join(table) + "\n")
 
 
 if __name__ == "__main__":
--- tools/small_rna_map/small_rna_map.r
+++ tools/small_rna_map/small_rna_map.r
@@ -1,4 +1,5 @@
 library(optparse)
+library(ggplot2)
 
 option_list <- list(
     make_option(c("-r", "--output_tab"), type="character", help="path to tabular file"),
@@ -11,5 +12,14 @@ args = parse_args(parser)
 Table = read.delim(args$output_tab, header=T, row.names=NULL)
 
 pdf(file=args$output_pdf, paper="special", height= 11.69, width = 8.2677)
-plot(Table)
+for (read in unique(Table$Chromosome)) {
+          print(ggplot(Table[Table$Chromosome==read,], aes(x=Coordinate, y=Nbr_reads, fill=Polarity))+ 
+          geom_bar(stat="identity", color="black", position=position_dodge())+
+	    facet_grid(Chromosome ~ Dataset)+
+          theme(strip.text.x = element_text(size=8, angle=75),
+          strip.text.y = element_text(size=12, face="bold"),
+          strip.background = element_rect(colour="red", fill="#CCCCFF"))+
+          geom_text(aes(label=Nbr_reads), vjust=1.2, color="white", position =position_dodge(0.9),size=3)) 
+	}
+
 dev.off()
