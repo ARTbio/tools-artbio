2a16568aff5dc867b806c240910155b50827da9d
chamaelj <c.eljaouhari@gmail.com>
2017-05-16 19:33:59 +0200

    sorting columns

--- tools/small_rna_map/test/small_rna_map.bash
+++ tools/small_rna_map/test/small_rna_map.bash
@@ -2,10 +2,9 @@
 # Executing this script require bash v4
 # Execution command : bash small_rna_map.bash
 
-sort -k 3 -k 4  samfile.sam -o samfile.sam
 
 while read Line
-do 
+do
    if [[ ${Line:0:1} != "@" ]] ; then
        echo "$Line" >> output1.tab
    elif [[ ${Line:0:3} = "@SQ" ]] ; then
@@ -14,7 +13,7 @@ do
 done < samfile.sam
 
 while read qname flag rname pos mapq cigar mrnm mpos isize seq qual opt
-do 
+do
    if [[ $flag = 0 ]] ; then
        polarity="F"
    elif [[ $flag = 16 ]] ;
@@ -22,41 +21,38 @@ do
        polarity="R"
    fi
    if [[ $flag = 0 ]] || [[ $flag = 16 ]] ; then
-       echo  "$rname   $pos    $polarity   ${#seq}" >> output2.tab
+       echo -e "$rname\t$pos\t$polarity\t${#seq}" >> output2.tab
    fi
 done < output1.tab
 rm output1.tab
 
 while read SQ SN LN
-do 
+do
     declare -A tab
     tab["${SN:3}"]=${LN:3};
 done < header.tab
 rm header.tab
-#echo "Nombre d'éléments : "${#tab[*]} ;
-# rm header.tab
+
 
 while read rname coord polarity qlen
-do  
+do
     key=$rname$coord$polarity
     len="${tab[$rname]}"
-    echo "$rname    $len    $coord  $qlen   $polarity   $key" >> output3.tab
-   
+    echo -e "$rname\t$len\t$coord\t$qlen\t$polarity\t$key" >> output3.tab
 done < output2.tab
 rm output2.tab
 
-echo -e "Chromosome\tChrom_length\tCoordinate\tNbr_reads\tPolarity" > output.tab 
+echo -e "Chromosome\tChrom_length\tCoordinate\tNbr_reads\tPolarity" > output.tab
 
 while read rname rlen coord qlen pol keys
-do  
-
+do
     if [[ $key != $keys ]] ; then
         key=$keys
         nbr_reads=`cat output3.tab | grep "$key" | wc -l`
-        echo -e "$rname\t$rlen\t$coord\t$nbr_reads\t$pol" >> output.tab 
+        echo -e "$rname\t$rlen\t$coord\t$nbr_reads\t$pol" >> output.tab
     fi
 done < output3.tab
 rm output3.tab
 
-sort -k 1 -k 3 -k 4  output.tab -o output.tab
+sort -k 1,1 -k 3,3n -k 5,5  output.tab -o output.tab
 
