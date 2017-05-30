1c915a0e9d2ffb17b54e5073a88e977ea5240697
chamaelj <c.eljaouhari@gmail.com>
2017-05-30 18:28:47 +0200

    this script generate pdf file containing Lollipop Chart which represent the number of read per position for each Chromosome

--- tools/small_rna_map/test.r
+++ tools/small_rna_map/test.r
@@ -1,15 +1,31 @@
 # Table is the data frame
-p <- ggplot(Table, aes(x=`Nbr_reads`, y=Coordinate, label=Nbr_reads)) + 
-  geom_point(stat='identity', fill="black", size=6)  +
-  geom_segment(aes(y = 0, 
-                   x = `Nbr_reads`, 
-                   yend = Coordinate, 
-                   xend = `Nbr_reads`), 
-               color = "black") +
-  geom_text(color="white", size=2) +
-  facet_wrap(~Chromosome, scales="free")+
-  coord_flip()
+library("ggplot2")
+library("gridExtra")
+library(ggplus)
 
-p
+theme_set(theme_bw())
+#Table=read.delim(your_input, header=T, row.names=NULL)
+Table <- within(Table[1:27,], Nbr_reads[Polarity=="R"] <- (Nbr_reads[Polarity=="R"]*-1)) 
+p <- ggplot(Table, aes(x=Coordinate, y=Nbr_reads, label=Nbr_reads, colour=Polarity), xlim=Chrom_length) +
+  geom_segment(aes(y = 0, 
+                   x = Coordinate, 
+                   yend = Nbr_reads, 
+                   xend = Coordinate)
+               ) +
+  facet_wrap(Dataset~Chromosome, scales="free")+
+  geom_hline(yintercept=0)+
+  ggplus::theme_readable(legend_position = 'right')
+
+
+plot.list <- by(data     = Table,
+                INDICES  = c(Table$Chromosome),
+                simplify = TRUE,
+                FUN      = function(x) {
+                  p %+% x 
+                })
+
+multi.plot <- marrangeGrob(grobs = plot.list,nrow  = 4, ncol = 1);
+ggsave(file="test.pdf", plot=multi.plot, height=11.69, width=8.2677)
+
 
 
