49fbc5c8fbe190c7d9aef10b3e97de943f80c9f0
chamaelj <c.eljaouhari@gmail.com>
2017-05-31 18:30:45 +0200

    Removed page numbers from marrangeGrob, and fixed xlim between 0 and the max of Chrom_length column

--- tools/small_rna_map/test.r
+++ tools/small_rna_map/test.r
@@ -6,16 +6,16 @@ library(ggplus)
 theme_set(theme_bw())
 #Table=read.delim(your_input, header=T, row.names=NULL)
 Table <- within(Table[1:27,], Nbr_reads[Polarity=="R"] <- (Nbr_reads[Polarity=="R"]*-1)) 
-p <- ggplot(Table, aes(x=Coordinate, y=Nbr_reads, label=Nbr_reads, colour=Polarity), xlim=Chrom_length) +
+p <- ggplot(Table, aes(x=Coordinate, y=Nbr_reads, label=Nbr_reads, colour=Polarity)) +
+#  xlim(0,Table[Chromosome,"Chrom_length"])+
   geom_segment(aes(y = 0, 
                    x = Coordinate, 
                    yend = Nbr_reads, 
                    xend = Coordinate)
                ) +
   facet_wrap(Dataset~Chromosome, scales="free")+
-  geom_hline(yintercept=0)+
-  ggplus::theme_readable(legend_position = 'right')
-
+  expand_limits(x= c(0, unique(Table[Table$Chromosome,"Chrom_length"])))+
+  geom_hline(yintercept=0)
 
 plot.list <- by(data     = Table,
                 INDICES  = c(Table$Chromosome),
@@ -24,7 +24,7 @@ plot.list <- by(data     = Table,
                   p %+% x 
                 })
 
-multi.plot <- marrangeGrob(grobs = plot.list,nrow  = 4, ncol = 1);
+multi.plot <- marrangeGrob(grobs = plot.list,nrow  = 4, ncol = 1, top=NULL);
 ggsave(file="test.pdf", plot=multi.plot, height=11.69, width=8.2677)
 
 
