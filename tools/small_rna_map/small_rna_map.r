7b6eb34674c8f8138d887ef22ed94b3acd1b9bef
chamaelj <c.eljaouhari@gmail.com>
2017-06-02 16:49:14 +0200

    plotting Lollipop Chart without using loops

--- tools/small_rna_map/small_rna_map.r
+++ tools/small_rna_map/small_rna_map.r
@@ -1,6 +1,7 @@
 library(optparse)
 library(ggplot2)
 library(gridExtra)
+library(RColorBrewer)
 
 option_list <- list(
     make_option(c("-r", "--output_tab"), type="character", help="path to tabular file"),
@@ -12,29 +13,36 @@ args = parse_args(parser)
 
 theme_set(theme_bw())
 Table = read.delim(args$output_tab, header=T, row.names=NULL)
-
 Table <- within(Table, Nbr_reads[Polarity=="R"] <- (Nbr_reads[Polarity=="R"]*-1))
 
+myColors <- brewer.pal(3,"Set1")
+names(myColors) <- levels(Table$Polarity)
+colScale <- scale_colour_manual(name = "Polarity",values = myColors)
 
-i=1
-plots <- list()
-for (read in unique(Table$Chromosome,Table$Dataset)) {
-  
-  bp <- ggplot(Table[Table$Chromosome==read,], 
-  aes(x=Coordinate, y=Nbr_reads, color=Polarity))+ 
+p <- ggplot(Table, aes(x=Coordinate, y=Nbr_reads, colour=Polarity)) +
+  colScale+
   geom_segment(aes(y = 0, 
                    x = Coordinate, 
                    yend = Nbr_reads, 
-                   xend = Coordinate), size=1)+
+                   xend = Coordinate,
+			 color=Polarity),
+			 alpha=1
+               ) +
+  geom_segment(aes(y = -Nbr_reads, 
+			 x = 0,
+			 yend=Nbr_reads,
+			 xend=Chrom_length), alpha=0
+		   )+
   facet_wrap(Dataset~Chromosome, scales="free")+
-  geom_hline(yintercept=0)
-  
-  plots[[i]] <- bp+scale_x_continuous(limits=c(0,Table$Chrom_length[i]))
-  i=i+1
-}
+  geom_hline(yintercept=0, size=0.3)
 
-p<-do.call(marrangeGrob,list(grobs=plots,ncol=1,nrow=4,top=NULL));
-ggsave(file=args$output_pdf, plot=p, height=11.69, width=8)
+plot.list <- by(data     = Table,
+                INDICES  = c(Table$Chromosome),
+                simplify = TRUE,
+                FUN      = function(x) {
+                  p %+% x 
+                })
 
-
+multi.plot <- marrangeGrob(grobs = plot.list,nrow  = 4, ncol = 1, top=NULL);
+ggsave(file=args$output_pdf, plot=multi.plot, height=11.69, width=8)
 
