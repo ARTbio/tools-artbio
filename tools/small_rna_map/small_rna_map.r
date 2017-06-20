a3c3293dbcdbd4207755f8c12de81331d5a566b4
chamaelj <c.eljaouhari@gmail.com>
2017-06-20 12:59:39 +0000

    plot one legend per page

--- tools/small_rna_map/small_rna_map.r
+++ tools/small_rna_map/small_rna_map.r
@@ -28,7 +28,7 @@ p <- ggplot(Table, aes(x=Coordinate, y=Nbr_reads, colour=Polarity)) +
 			 color=Polarity),
 			 alpha=1
                ) +
-  geom_segment(aes(y = -Nbr_reads, 
+  geom_segment(aes(y = Nbr_reads, 
 			 x = 0,
 			 yend=Nbr_reads,
 			 xend=Chrom_length), alpha=0
@@ -36,11 +36,20 @@ p <- ggplot(Table, aes(x=Coordinate, y=Nbr_reads, colour=Polarity)) +
   facet_wrap(Dataset~Chromosome, scales="free", nrow=1, labeller = label_wrap_gen(multi_line = FALSE))+
   scale_y_continuous(breaks = function(x) round(pretty(seq(-(max(x) + 1), (max(x) + 1)))))+
   geom_hline(yintercept=0, size=0.3)+
-  theme(strip.text = element_text(size = 8, lineheight = 0.1), 
+  theme(strip.text = element_text(size = 6, lineheight = 0.1), 
         panel.grid.major = element_line(colour = "#ffffff"),
         panel.grid.minor = element_line(colour = "#ffffff"),
-        axis.title = element_blank())
-  
+        axis.title = element_blank(),
+        axis.text = element_text(size = 6))
+#extract legend
+g_legend<-function(a.gplot){
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)}
+
+mylegend <- g_legend(p);
+p <- p+ theme(legend.position = "none")
 
 plot.list <- by(data     = Table,
                 INDICES  = c(Table$Chromosome),
@@ -49,6 +58,6 @@ plot.list <- by(data     = Table,
                   p %+% x 
                 })
 
-multi.plot <- marrangeGrob(grobs = plot.list,nrow  = 8, ncol = 1, top=NULL, bottom="Coordinate", left="Number of reads");
+multi.plot <- marrangeGrob(grobs = plot.list,nrow  = 8, ncol = 1, top=NULL, bottom="Coordinates(nt)", left="Number of reads", right = mylegend);
 ggsave(args$output_pdf, device="pdf", plot=multi.plot, height=11.69, width=8.2)
 
