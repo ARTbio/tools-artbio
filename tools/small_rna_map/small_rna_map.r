8793d1fb5f22dbec6f2bd6c51c1393caf787af2b
chamaelj <c.eljaouhari@gmail.com>
2017-06-22 12:57:28 +0000

    annotate R script

--- tools/small_rna_map/small_rna_map.r
+++ tools/small_rna_map/small_rna_map.r
@@ -11,14 +11,16 @@ option_list <- list(
 parser <- OptionParser(usage = "%prog [options] file", option_list = option_list)
 args = parse_args(parser)
 
-theme_set(theme_bw())
+theme_set(theme_bw()) #a theme with a white background
 Table = read.delim(args$output_tab, header=T, row.names=NULL)
 Table <- within(Table, Nbr_reads[Polarity=="R"] <- (Nbr_reads[Polarity=="R"]*-1))
 
+#To assign colors to categorical variables in ggplot2 that have stable mapping
 myColors <- brewer.pal(3,"Set1")
 names(myColors) <- levels(Table$Polarity)
 colScale <- scale_colour_manual(name = "Polarity",values = myColors)
 
+#Make initial figures
 p <- ggplot(Table, aes(x=Coordinate, y=Nbr_reads, colour=Polarity)) +
   colScale+
   geom_segment(aes(y = 0, 
@@ -34,13 +36,13 @@ p <- ggplot(Table, aes(x=Coordinate, y=Nbr_reads, colour=Polarity)) +
 			 xend=Chrom_length), alpha=0
 		   )+
   facet_wrap(Dataset~Chromosome, scales="free", nrow=1, labeller = label_wrap_gen(multi_line = FALSE))+
-  scale_y_continuous(breaks = function(x) round(pretty(seq(-(max(x) + 1), (max(x) + 1)))))+
+  scale_y_continuous(breaks = function(x) round(pretty(seq(-(max(x) + 1), (max(x) + 1)))))+#to display only integer values on y axis
   geom_hline(yintercept=0, size=0.3)+
-  theme(strip.text = element_text(size = 6, lineheight = 0.1), 
-        panel.grid.major = element_line(colour = "#ffffff"),
-        panel.grid.minor = element_line(colour = "#ffffff"),
-        axis.title = element_blank(),
-        axis.text = element_text(size = 6))
+  theme(strip.text = element_text(size = 6, lineheight = 0.1), #specify strip size
+        panel.grid.major = element_line(colour = "#ffffff"),#conceal major grid lines
+        panel.grid.minor = element_line(colour = "#ffffff"),#conceal minor grid lines
+        axis.title = element_blank(),# Conceal axis titles
+        axis.text = element_text(size = 6))#modify the size of tick labels along axes
 #extract legend
 g_legend<-function(a.gplot){
   tmp <- ggplot_gtable(ggplot_build(a.gplot))
@@ -49,15 +51,16 @@ g_legend<-function(a.gplot){
   return(legend)}
 
 mylegend <- g_legend(p);
-p <- p+ theme(legend.position = "none")
 
+p <- p+ theme(legend.position = "none")# Hide the repeated caption
+#transforme ggplot graphs on list of graphs
 plot.list <- by(data     = Table,
                 INDICES  = c(Table$Chromosome),
                 simplify = TRUE,
                 FUN      = function(x) {
                   p %+% x 
                 })
-
+#Plotting in multiple pages with different rows
 multi.plot <- marrangeGrob(grobs = plot.list,nrow  = 8, ncol = 1, top=NULL, bottom="Coordinates(nt)", left="Number of reads", right = mylegend);
 ggsave(args$output_pdf, device="pdf", plot=multi.plot, height=11.69, width=8.2)
 
