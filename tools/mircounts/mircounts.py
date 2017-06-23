3d85754193e455a80d7ca18e84d3dd7e77b64cc2
ccedmendoza <cedricmendoza@gmail.com>
2017-06-23 16:34:53 +0200

    Made the modifications asked for in last commit

--- tools/mircounts/mircounts.py
+++ tools/mircounts/mircounts.py
@@ -10,34 +10,33 @@ def option_parsing():
     parser.add_option('-m', '--mirs', action='store_true', dest='mirs', help='Count mature miRNAs', default=False)
     inputs = optparse.OptionGroup(parser, 'Inputs')
     inputs.add_option('--alignment', type='string', dest='alignment_file', help='Alignment tabular or sam file')
-    inputs.add_option('--gff', type='string', dest='gff_file', help=' GFF3 describing both pre-mirs and mature mirs (to be discussed)', metavar='FILE')
+    inputs.add_option('--gff', type='string', dest='gff_file', help='GFF3 describing both pre-miRNAs and mature miRNAs', metavar='FILE')
     parser.add_option_group(inputs)
     outputs = optparse.OptionGroup(parser, 'Outputs')
-    outputs.add_option('--pre_mirs_output', type='string', dest='output_pre_mirs', help='GFF3 describing the mature miRs', metavar='FILE', default='output_pre_mirs_count.tab')
-    outputs.add_option('--mature_mirs_output', type='string', dest='output_mature_mirs', help='Reference genome', metavar='FILE', default='output_mirs_count.tab')
-    outputs.add_option('--lattice', type='string', dest='lattice')
+    outputs.add_option('--pre_mirs_output', type='string', dest='output_pre_mirs', help='Output file containing table containing the number of hits per pre-miRNA', metavar='FILE', default='output_pre_mirs_count.tab')
+    outputs.add_option('--mature_mirs_output', type='string', dest='output_mature_mirs', help='Output file containing the number of hits per mature miRNA', metavar='FILE', default='output_mirs_count.tab')
+    outputs.add_option('--lattice', type='string', dest='lattice', help="Output file for the lattice dataframe (if none given it won't be outputed)")
     parser.add_option_group(outputs)
     (options, args) = parser.parse_args()
     """ Check if the options were correctly passed """
     if len(args) > 0:
-        parser.error('Wrong number of arguments')
+        parser.error('Please use the options flags to pass your arguments')
     if (not options.alignment_file or not options.gff_file):
         parser.error("Missing file. Both '--alignment' and '--gff' files are needed")
-    if (options.lattice and not options.mirs):
-        parse.error("Can't output lattice dataframe if the option '-m' is not set")
     return options
 
-def get_count(bamfile):
+def get_pre_mir_counts(bamfile):
     """
     Takes a AlignmentFile object and and returns a count of reads
-    hiting 'miRNA_primary_transcript' positions
+    hiting 'miRNA_primary_transcript' positions and the coverage of said region
     """
     count = dict()
-    for reference in bamfile.header['SQ']:
-        count[reference['SN']] = [bamfile.count(reference['SN'])]
+    for ref in bamfile.references:
+        count[ref] = [bamfile.count(ref),
+                      bamfile.count_coverage(ref)]
     return count
 
-def get_coverage(bamfile, gff_file):
+def get_mir_counts(bamfile, gff_file):
     """
     Takes a AlignmentFile and a gff file and computes for
     each 'miRNA' region of the gff the number of reads that hit it
@@ -53,7 +52,6 @@ def get_coverage(bamfile, gff_file):
                 if gff_fields[2] == 'miRNA':
                     counts[gff_fields[0]] = [bamfile.count(reference=gff_fields[0], start=int(gff_fields[3]),
                                                            end=int(gff_fields[4])),
-                                             gff_fields[6],
                                              bamfile.count_coverage(reference=gff_fields[0], start=int(gff_fields[3]),
                                                                     end=int(gff_fields[4]))]
         gff.close()
@@ -64,31 +62,32 @@ def get_coverage(bamfile, gff_file):
 
 def write_dataframe(mirs, outfile, sample):
     """
-    Takes a dictionnary dict[reference name] = [polarity, count,
+    Takes a dictionnary dict[reference name] = [count,
                                                 [[Hits of each position for 'A'],
                                                  [Hits of each position for 'C'],
                                                  [Hits of each position for 'G'],
-                                                 [Hits of each position for 'A']]]
-    And prints a dataframe with columns: sample, mir, offset, offsetNorm, counts, countsNorm, polarity
+                                                 [Hits of each position for 'T']]]
+    And prints a dataframe with columns: sample, mir, offset, offsetNorm, counts, countsNorm
     in the outfile
     """
     dataframe = []
-    dataframe.append("sample\tmir\toffset\toffsetNorm\tcounts\tcountsNorm\tpolarity")
+    dataframe.append("sample\tmir\toffset\toffsetNorm\tcounts\tcountsNorm")
     for ref in mirs.keys():
         """ For each reference name in mirs write the coverage of each of its positions """
-        maximum = max(mirs[ref][2][0])
+        maximum = 0
+        coverage_arrays = mirs[ref][1]
+        reference_length = len(coverage_arrays[1])
         for i in range(4):
-            if max(mirs[ref][2][i]):
-                maximum = max(mirs[ref][2][i])
-        for pos in range(len(mirs[ref][2][1])):
+            if max(coverage_arrays[i]):
+                maximum = max(coverage_arrays[i])
+        for pos in range(reference_length):
             """ Compute coverage of each position and append to the dataframe a new line"""
-            coverage = mirs[ref][2][0][pos]+mirs[ref][2][1][pos]+mirs[ref][2][2][pos]+mirs[ref][2][3][pos]
+            coverage = coverage_arrays[0][pos] + coverage_arrays[1][pos] + coverage_arrays[2][pos] + coverage_arrays[3][pos]
             dataframe.append("\t".join([sample, ref,
-                                        str(pos+1), # offset
-                                        str(float((pos+1))/len(mirs[ref][2][1])), # offsetNorm
+                                        str(pos+1), # offset + 1 because range starts from 0
+                                        str(float((pos+1))/reference_length), # offsetNorm
                                         str(coverage), # count
-                                        str(float(coverage)/float(maximum)) if maximum >0 else '0', # countNorm
-                                        mirs[ref][1]])) # polarity
+                                        str(float(coverage)/float(maximum)) if maximum >0 else '0'])) # countNorm
     try:
         out = open(outfile, 'w')
         out.write("\n".join(dataframe))
@@ -121,13 +120,15 @@ def __main__():
     pre_mirs = dict()
     mirs = dict()
     if options.pre_mirs:
-        pre_mirs = get_count(bamfile)
+        pre_mirs = get_pre_mir_counts(bamfile)
         write_counts(pre_mirs, options.output_pre_mirs)
+        if options.lattice:
+            write_dataframe(pre_mirs, options.lattice, options.alignment_file)
     if options.mirs:
-        mirs = get_coverage(bamfile, options.gff_file)
+        mirs = get_mir_counts(bamfile, options.gff_file)
         write_counts(mirs, options.output_mature_mirs)
-    if options.lattice:
-        write_dataframe(mirs, options.lattice, options.alignment_file)
+        if options.lattice:
+            write_dataframe(mirs, options.lattice, options.alignment_file)
 
 if __main__():
     __main__()
