5d1595d797187418e26bfbe1c9da43cff80e14e8
chamaelj <c.eljaouhari@gmail.com>
2017-07-05 12:27:29 +0000

    add y second y axis in the right of the plots

--- tools/small_rna_map/small_rna_map.r
+++ tools/small_rna_map/small_rna_map.r
@@ -3,24 +3,25 @@ library(ggplot2)
 library(gridExtra)
 library(RColorBrewer)
 library(gtable)
-
+library(grid)
+ 
 option_list <- list(
     make_option(c("-r", "--output_tab"), type="character", help="path to tabular file"),
     make_option("--output_pdf", type = "character", help="path to the pdf file with plot")
     )
-
+ 
 parser <- OptionParser(usage = "%prog [options] file", option_list = option_list)
 args = parse_args(parser)
-
+ 
 theme_set(theme_bw()) #a theme with a white background
 Table = read.delim(args$output_tab, header=T, row.names=NULL)
 Table <- within(Table, Nbr_reads[Polarity=="R"] <- (Nbr_reads[Polarity=="R"]*-1))
-
+ 
 #To assign colors to categorical variables in ggplot2 that have stable mapping
 myColors <- brewer.pal(3,"Set1")
 names(myColors) <- levels(Table$Polarity)
 colScale <- scale_colour_manual(name = "Polarity",values = myColors)
-
+ 
 #Make initial figures
 p <- ggplot(Table, aes(x=Coordinate, y=Nbr_reads, colour=Polarity)) +
   colScale+
@@ -50,28 +51,31 @@ g_legend<-function(a.gplot){
   leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
   legend <- tmp$grobs[[leg]]
   return(legend)}
-
+ 
 mylegend <- g_legend(p);
-
+ 
 p <- p+ theme(legend.position = "none")# Hide the repeated caption
-
+ 
 # The second plot
-p2 <- ggplot(Table, aes(x = Coordinate, y = Median)) +
+p2 <- ggplot(Table, aes(x = Coordinate, y = Median, group=1)) +
   geom_point(aes(col=Median),size = 1, colour="black") + 
   geom_line()+
-  expand_limits(y = seq(0,max(Table$Median),by=4)) +
-  facet_wrap(Dataset~Chromosome, scales="free") +
+  expand_limits(y = seq(0,max(Table$Median),by=5)) +
+  facet_wrap(Dataset~Chromosome, scales="free", nrow=1, labeller = label_wrap_gen(multi_line = FALSE))+
   geom_segment(aes(y = Nbr_reads, 
 			 x = 0,
 			 yend=Nbr_reads,
 			 xend=Chrom_length), alpha=0
 		   )+
-  theme(panel.background = element_rect(fill = NA),
+  scale_y_continuous(limits = c(0,max(Table$Median)), position = "right")+
+  theme(strip.text = element_text(size = 6, lineheight = 0.1),
+        panel.background = element_rect(fill = NA),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.border = element_rect(fill = NA, colour = "grey50"),
-        legend.position = "bottom")
-
+        axis.text = element_text(size = 6),
+        legend.position = "bottom"
+        )
 # Transforme ggplot graphs on list of graphs
 plot.list1 <- by(data     = Table,
                 INDICES  = c(Table$Chromosome),
@@ -86,35 +90,71 @@ plot.list2 <- by(data     = Table,
                 FUN      = function(x) {
                   p2 %+% x 
                 })
-
+ 
+# A function to get the original tick mark length
+plot_theme <- function(p) {
+  plyr::defaults(p$theme, theme_get())
+}
+ 
+# ggplot contains many labels that are themselves complex grob; 
+# usually a text grob surrounded by margins.
+# When moving the grobs from, say, the left to the right of a plot,
+# Make sure the margins and the justifications are swapped around.
+# The function below does the swapping.
+# Taken from the cowplot package:
+# https://github.com/wilkelab/cowplot/blob/master/R/switch_axis.R 
+ 
+hinvert_title_grob <- function(grob){
+ 
+  # Swap the widths
+  widths <- grob$widths
+  grob$widths[1] <- widths[3]
+  grob$widths[3] <- widths[1]
+  grob$vp[[1]]$layout$widths[1] <- widths[3]
+  grob$vp[[1]]$layout$widths[3] <- widths[1]
+ 
+  # Fix the justification
+  grob$children[[1]]$hjust <- 1 - grob$children[[1]]$hjust 
+  grob$children[[1]]$vjust <- 1 - grob$children[[1]]$vjust 
+  grob$children[[1]]$x <- unit(1, "npc") - grob$children[[1]]$x
+  grob
+}
+ 
 dual_axis <- function(v1,v2){
 # Get the ggplot grobs
-			g1 <- ggplot_gtable(ggplot_build(v1))
-			g2 <- ggplot_gtable(ggplot_build(v2))
+	g1 <- ggplot_gtable(ggplot_build(v1))
+	g2 <- ggplot_gtable(ggplot_build(v2))
 # Get the locations of the plot panels in g1.
-			pp <- c(subset(g1$layout, grepl("panel", g1$layout$name), se = t:r))
+	pp <- c(subset(g1$layout, grepl("panel", g1$layout$name), se = t:r))
 # Overlap panels for second plot on those of the first plot
-			g <- gtable_add_grob(g1, g2$grobs[grepl("panel", g1$layout$name)], 
-      			pp$t, pp$l, pp$b, pp$l)
-# Extract the y axis of the second plot
-			ia<-which(g2$layout$name=="axis-l-1-1")
-			ga <- g2$grobs[[ia]]
-			ax <- ga$children[[2]]
-#Flip it horizontally
-			ax$widths <- rev(ax$widths)
-			ax$grobs <- rev(ax$grobs)
-#Add the flipped y-axis to the right
-			g <- gtable_add_cols(g, g2$widths[g2$layout[ia, ]$l], length(g$widths) - 1)
-			g <- gtable_add_grob(g, ax, pp$t, length(g$widths) - 1, pp$b)
-		}
+	g <- gtable_add_grob(g1, g2$grobs[grepl("panel", g1$layout$name)], 
+      	pp$t, pp$l, pp$b, pp$l)
+# Get the y axis from g2 (axis line, tick marks, and tick mark labels)
+	index <- which(g2$layout$name == "axis-r-1-1")  # Which grob.   
+	yaxis <- g2$grobs[[index]]      # Extract the grob
+	ticks <- yaxis$children[[2]]  # swap tick marks and tick mark labels
+ 
+# Move the tick marks, Tick mark lengths can change. 
+	tml <- plot_theme(p)$axis.ticks.length   # Tick mark length
+	ticks$grobs[[1]]$x <- ticks$grobs[[1]]$x - unit(1, "npc") + tml
+ 
+# Swap margins and fix justifications for the tick mark labels
+ticks$grobs[[2]] <- hinvert_title_grob(ticks$grobs[[2]])
+ 
+# Put ticks back into yaxis
+yaxis$children[[2]] <- ticks
+ 
+# Put the transformed yaxis on the right side of g1
+g <- gtable_add_cols(g, g2$widths[g2$layout[index, ]$l], max(pp$r))
+g <- gtable_add_grob(g, yaxis, max(pp$t), max(pp$r) + 1, max(pp$b), max(pp$r) + 1, 
+   clip = "off")
+ 
+}
  
 plots <- list()
- 
- 
-
 len = length(plot.list1)
 for(i in 1:len ) {plots[[i]] <- dual_axis(plot.list1[[i]],plot.list2[[i]])}
 # Plotting in multiple pages with different rows
-multi.plot<-do.call(marrangeGrob,list(grobs=plots,ncol=1,nrow=8,top=NULL, bottom="Coordinates(nt)", left="Number of reads", right = mylegend));
+multi.plot<-do.call(marrangeGrob,list(grobs=plots,ncol=1,nrow=8,top=NULL, bottom="Coordinates(nt)", left="Number of reads", right= mylegend))
 ggsave(args$output_pdf, device="pdf", plot=multi.plot, height=11.69, width=8.2)
 
