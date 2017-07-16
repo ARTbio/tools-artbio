e0fe64427b7c2bfbd62fd3c4c96850708bd3cade
drosofff <drosofff@gmail.com>
2017-07-16 15:15:12 +0200

    release first part of tool redone with lattice package that outperforms by far ggplot2

--- tools/small_rna_map/lattice_small_rna_map.r
+++ tools/small_rna_map/lattice_small_rna_map.r
@@ -1,7 +1,7 @@
 ## Setup R error handling to go to stderr
-options( show.error.messages=F,
-       error = function () { cat( geterrmessage(), file=stderr() ); q( "no", 1, F ) } )
-
+#options( show.error.messages=F,
+#       error = function () { cat( geterrmessage(), file=stderr() ); q( "no", 1, F ) } )
+warnings()
 library(RColorBrewer)
 library(lattice)
 library(latticeExtra)
@@ -24,14 +24,14 @@ Table <- within(Table, Nbr_reads[Polarity=="R"] <- (Nbr_reads[Polarity=="R"]*-1)
 n_samples=length(unique(Table$Dataset))
 genes=unique(levels(Table$Chromosome))
 per_gene_readmap=lapply(genes, function(x) subset(Table, Chromosome==x))
+per_gene_limit=lapply(genes, function(x) c(1, unique(subset(Table, Chromosome==x)$Chrom_length)) )
 n_genes=length(per_gene_readmap)
-
 ## end of data frames implementation
 
 ## functions
 
 plot_readmap=function(df, ...) {
-combineLimits(xyplot(Nbr_reads~Coordinate|factor(Dataset, levels=unique(Dataset))+reorder(Chromosome, Nbr_reads, function(x) -sum(abs(x))),
+combineLimits(xyplot(Nbr_reads~Coordinate|factor(Dataset, levels=unique(Dataset))+factor(Chromosome, levels=unique(Chromosome)),
 data=df,
 type='h',
 scales= list(relation="free", x=list(rot=0, cex=0.7, axs="i", tck=0.5), y=list(tick.number=4, rot=90, cex=0.7)),
@@ -49,7 +49,7 @@ par.strip.text = list(cex=0.7),
 
 ## function parameters'
 
-par.settings.readmap=list(layout.heights=list(top.padding=0, bottom.padding=-2.5), strip.background = list(col=c("lightblue","lightgreen")) )
+par.settings.readmap=list(layout.heights=list(top.padding=0, bottom.padding=0), strip.background = list(col=c("lightblue","lightgreen")) )
 
 ## end of function parameters'
 
@@ -57,7 +57,7 @@ par.settings.readmap=list(layout.heights=list(top.padding=0, bottom.padding=-2.5
 
 if (n_genes > 7) {page_height_simple = 11.69; page_height_combi=11.69; rows_per_page=8} else {
                  rows_per_page= n_genes; page_height_simple = 2.5*n_genes; page_height_combi=page_height_simple*2 }
-if (n_samples > 4) {page_width = 8.2677*n_samples/4} else {page_width = 8.2677*n_samples/3} # to test
+if (n_samples > 4) {page_width = 8.2677*n_samples/4} else {page_width = 8.2677*n_samples/2} # to test
 
 
 pdf(file=args$output_pdf, paper="special", height=page_height_simple, width=page_width)
@@ -65,7 +65,7 @@ for (i in seq(1,n_genes,rows_per_page)) {
 start=i
 end=i+rows_per_page-1
 if (end>n_genes) {end=n_genes}
-readmap_plot.list=lapply(per_gene_readmap[start:end], function(x) plot_readmap(x, par.settings=par.settings.readmap))
+readmap_plot.list=lapply(per_gene_readmap[start:end], function(x) plot_readmap(x, xlim=c(1, x$Chrom_length[1]), par.settings=par.settings.readmap))
 args_list=c(readmap_plot.list, list(nrow=rows_per_page, ncol=1,
                                     top=textGrob("Read Maps (nucleotide coordinates)", gp=gpar(cex=1), just="top"),
                                     left=textGrob("Read counts", gp=gpar(cex=1), vjust=1, rot=90)
@@ -74,3 +74,7 @@ args_list=c(readmap_plot.list, list(nrow=rows_per_page, ncol=1,
 do.call(grid.arrange, args_list)
 }
 devname=dev.off()
+#op <- options(nwarnings = 10000) ## <- get "full statistics"
+#summary(warnings())
+#options(op) # revert to previous (keeping 50 messages by default)
+
