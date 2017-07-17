856e9817182174874805328c5979c1d8bbd89c67
drosofff <drosofff@gmail.com>
2017-07-17 17:17:35 +0200

    some tests on mircounts.py, needed transciently a clipped.out.bam.bai file

--- tools/mircounts/mircounts.py
+++ tools/mircounts/mircounts.py
@@ -6,18 +6,32 @@ import sys
 
 def option_parsing():
     parser = optparse.OptionParser(description='Count miRNAs')
-    parser.add_option('-p', '--pre_mirs', action='store_true', dest='pre_mirs', help='Count pre-miRNAs', default=False)
-    parser.add_option('-m', '--mirs', action='store_true', dest='mirs', help='Count mature miRNAs', default=False)
+    parser.add_option('-p', '--pre_mirs', action='store_true',
+                      dest='pre_mirs', help='Count pre-miRNAs', default=False)
+    parser.add_option('-m', '--mirs', action='store_true', dest='mirs',
+                      help='Count mature miRNAs', default=False)
     inputs = optparse.OptionGroup(parser, 'Inputs')
-    inputs.add_option('--alignment', type='string', dest='alignment_file', help='Alignment tabular or sam file')
-    inputs.add_option('--gff', type='string', dest='gff_file', help='GFF3 describing both pre-miRNAs and mature miRNAs', metavar='FILE')
-    inputs.add_option('--quality_threshold', type='int', dest='quality_threshold', help='Quality of the base alignment to take in to consideration when counting coverage (default=10)', default=10)
-    inputs.add_option('--sample', type='string', dest='sample_name', help='Sample name (default= sample1)', default='sample1')
+    inputs.add_option('--alignment', type='string', dest='alignment_file',
+                      help='Alignment tabular or sam file')
+    inputs.add_option('--gff', type='string', dest='gff_file',
+                      help='GFF3 describing both pre-miRNAs and mature miRNAs',
+                      metavar='FILE')
+    inputs.add_option('--quality_threshold', type='int', dest='quality_threshold',
+                      help='Quality threshold for coverage (default=10)',
+                      default=10)
+    inputs.add_option('--sample', type='string', dest='sample_name',
+                      help='Sample name (default= sample1)', default='sample1')
     parser.add_option_group(inputs)
     outputs = optparse.OptionGroup(parser, 'Outputs')
-    outputs.add_option('--pre_mirs_output', type='string', dest='output_pre_mirs', help='Output file containing table containing the number of hits per pre-miRNA', metavar='FILE', default='output_pre_mirs_count.tab')
-    outputs.add_option('--mature_mirs_output', type='string', dest='output_mature_mirs', help='Output file containing the number of hits per mature miRNA', metavar='FILE', default='output_mirs_count.tab')
-    outputs.add_option('--lattice', type='string', dest='lattice', help="Output file for the lattice dataframe (if none given it won't be outputed)")
+    outputs.add_option('--pre_mirs_output', type='string', dest='output_pre_mirs',
+                       help='pre-miRNA count file', metavar='FILE',
+                       default='output_pre_mirs_count.tab')
+    outputs.add_option('--mature_mirs_output', type='string',
+                       dest='output_mature_mirs',
+                       help='Mature miRNA counts file', metavar='FILE',
+                       default='output_mirs_count.tab')
+    outputs.add_option('--lattice', type='string', dest='lattice',
+                       help='Output file for the lattice dataframe. Default: None')
     parser.add_option_group(outputs)
     (options, args) = parser.parse_args()
     """ Check if the options were correctly passed """
@@ -64,7 +78,7 @@ def get_mir_counts(bamfile, gff_file, quality_th):
                 mir_start = int(gff_fields[3])
                 mir_end = int(gff_fields[4])
                 count = 0
-                # GFF is 1-based, pysam is 0-based
+                # GFF is 1-based, pysam is 0-based. Check This
                 for read in bamfile.fetch(premir_name, mir_start-1, mir_end-1):
                     count += 1
                 counts[mir_name] = count
