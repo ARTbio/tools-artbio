ac0333bbadb964c84a995c6f85f149b7d4aa47ce
drosofff <drosofff@gmail.com>
2017-07-18 00:50:45 +0200

    finished polishing mircounts.py. tests to fix

--- tools/mircounts/mircounts.py
+++ tools/mircounts/mircounts.py
@@ -1,43 +1,31 @@
 #!/usr/bin/env python
-import optparse
-from collections import defaultdict
-import pysam
-import sys
+import argparse
 
-def option_parsing():
-    parser = optparse.OptionParser(description='Count miRNAs')
-    parser.add_option('-p', '--pre_mirs', action='store_true',
-                      dest='pre_mirs', help='Count pre-miRNAs', default=False)
-    parser.add_option('-m', '--mirs', action='store_true', dest='mirs',
-                      help='Count mature miRNAs', default=False)
-    inputs = optparse.OptionGroup(parser, 'Inputs')
-    inputs.add_option('--alignment', type='string', dest='alignment_file',
-                      help='Alignment tabular or sam file')
-    inputs.add_option('--gff', type='string', dest='gff_file',
-                      help='GFF3 describing both pre-miRNAs and mature miRNAs',
-                      metavar='FILE')
-    inputs.add_option('--quality_threshold', type='int', dest='quality_threshold',
-                      help='Quality threshold for coverage (default=10)',
-                      default=10)
-    parser.add_option_group(inputs)
-    outputs = optparse.OptionGroup(parser, 'Outputs')
-    outputs.add_option('--pre_mirs_output', type='string', dest='output_pre_mirs',
-                       help='pre-miRNA count file', metavar='FILE',
-                       default='output_pre_mirs_count.tab')
-    outputs.add_option('--mature_mirs_output', type='string',
-                       dest='output_mature_mirs',
-                       help='Mature miRNA counts file', metavar='FILE',
-                       default='output_mirs_count.tab')
-    outputs.add_option('--lattice', type='string', dest='lattice',
-                       help='Output file for the lattice dataframe. Default: None')
-    parser.add_option_group(outputs)
-    (options, args) = parser.parse_args()
-    """ Check if the options were correctly passed """
-    if len(args) > 0:
-        parser.error('Please use the options flags to pass your arguments')
-    if (not options.alignment_file or not options.gff_file):
-        parser.error("Missing file. Both '--alignment' and '--gff' files are needed")
-    return options
+from collections import defaultdict
+
+import pysam
+
+
+def Parser():
+    parser = argparse.ArgumentParser(description='miRNAs counts and coverages')
+    parser.add_argument('-a', '--alignment', metavar='FILE', type=str,
+                        dest='alignment_file', help='Alignment bam file')
+    parser.add_argument('--gff', metavar='FILE', type=str, dest='gff_file',
+                        help='GFF3 describing both pre-miRNAs\
+                              and mature miRNAs')
+    parser.add_argument('-q', '--quality_threshold', type=int,
+                        dest='quality_threshold',
+                        help='Quality threshold for coverage (default=10)',
+                        default=10)
+    parser.add_argument('-p', '--pre_mirs', type=str, dest='pre_mirs',
+                        help='pre-miRNAs count file path', metavar='FILE')
+    parser.add_argument('-m', '--mirs', type=str, dest='mirs',
+                        help='mature miRNA count file path', metavar='FILE')
+    parser.add_argument('--lattice', metavar='FILE', type=str, dest='lattice',
+                        help='Output file for the lattice dataframe.')
+    args = parser.parse_args()
+    return args
+
 
 def get_pre_mir_counts(bamfile):
     """
@@ -46,23 +34,25 @@ def get_pre_mir_counts(bamfile):
     """
     count = dict()
     for ref_name in bamfile.references:
-        count[ref_name] = bamfile.count(reference = ref_name)
+        count[ref_name] = bamfile.count(reference=ref_name)
     return count
-    
-def get_pre_mir_coverage(bamfile, quality=10)
+
+
+def get_pre_mir_coverage(bamfile, quality=10):
     """
     Takes a AlignmentFile object and returns a dictionary of lists
     of coverage along the coordinates of pre_mirs (as keys)
     """
     coverage = dict()
     for ref_name, ref_len in zip(bamfile.references, bamfile.lengths):
-        coverage[ref_name]= bamfile.count_coverage(reference = ref_name,
-	                                               start=0, end = ref_len,
-	                                               quality_threshold =quality)]
+        coverage[ref_name] = bamfile.count_coverage(reference=ref_name,
+                                                    start=0, end=ref_len,
+                                                    quality_threshold=quality)
         """ Add the 4 coverage values """
         coverage[ref_name] = [sum(x) for x in
                               zip(*coverage[ref_name])]
     return coverage
+
 
 def get_mir_counts(bamfile, gff_file):
     """
@@ -71,21 +61,20 @@ def get_mir_counts(bamfile, gff_file):
     returns a dict[mir_name] = count
     """
     counts = defaultdict(int)
-    refs = bamfile.references
     for line in open(gff_file, 'r'):
         if line[0] != '#':
             gff_fields = line[:-1].split("\t")
             if gff_fields[2] == 'miRNA':
                 mir_name = gff_fields[0]
-                premir_name = gff_fields[8].split('=')[1]
+                premir_name = gff_fields[8].split('=')[-1]
                 mir_start = int(gff_fields[3])
                 mir_end = int(gff_fields[4])
-                count = 0
                 # GFF is 1-based, pysam is 0-based.
-                counts[mir_name] = bamfile.count(reference = mir_name,
-                                                 start = mir_start-1,
-                                                 end = mir_end-1)
+                counts[mir_name] = bamfile.count(reference=premir_name,
+                                                 start=mir_start-1,
+                                                 end=mir_end-1)
     return counts
+
 
 def write_dataframe_coverage(countdict, outfile):
     """
@@ -95,51 +84,48 @@ def write_dataframe_coverage(countdict, outfile):
     in the outfile
     """
     F = open(outfile, 'w')
-    F.write('Pre-Mir\tOffset\tNorm_offset\tCount\tNorm_count')
+    F.write('Pre-Mir\tOffset\tNorm_offset\tCount\tNorm_count\n')
     for ref in sorted(countdict):
-        """ For each reference name in mirs write the coverage of each of its positions """
-        max = max(countdict[ref])
+        """
+        For each reference name in mirs,
+        write the coverage of each of its positions
+        """
+        maximum = max(countdict[ref])
         reference_length = len(countdict[ref])
         for pos, c in enumerate(countdict[ref]):
             """ Compute and write value for each reference position"""
-            F.write('%s\t%s\t%s\t%s\t%s\t%s\n' % (ref,
-                                str(pos + 1),
-                                str(float(pos+1)/reference_length),
-                                str(c),
-                                str(float(c)/max) if max != 0 else '0'))
+            F.write('%s\t%s\t%s\t%s\t%s\n' % (ref, str(pos + 1),
+                    str(float(pos+1)/reference_length), str(c),
+                    str(float(c)/maximum) if maximum != 0 else '0'))
+    F.close()
 
-def write_counts(countdict, outfile, gene_type='mir'):
+
+def write_counts(countdict, outfile):
     """
-    Takes a dict[<gene_type name>]=count and 
+    Takes a dict[<gene_type name>]=count and
     writes a count table
     """
-    table = []
-    table.append("Gene\tCounts")
-    for gene in counts:
-        table.append("\t".join([gene, str(counts[gene][0])]))
-    try:
-        out = open(outfile, 'w')
-        out.write("\n".join(table))
-        out.write("\n")
-        out.close()
-    except IOError as e:
-        sys.stderr.write("Error while writing file %s\n" % outfile)
-        sys.stderr.write("I/O error(%s): %s\n" % (e.errno, e.strerror))
+    F = open(outfile, 'w')
+    F.write('Gene\tCounts\n')
+    for gene in sorted(countdict):
+        F.write('%s\t%s\n' % (gene, str(countdict[gene])))
+    F.close()
 
-def __main__():
-    options = option_parsing()
-    bamfile = pysam.AlignmentFile(options.alignment_file, 'rb', check_sq=False)
-    pre_mirs = dict()
-    mirs = dict()
-    if options.pre_mirs:
-        pre_mirs = get_pre_mir_counts(bamfile, options.quality_threshold)
-        write_counts(pre_mirs, options.output_pre_mirs)
-        if options.lattice:
-            write_dataframe(pre_mirs, options.lattice, options.sample_name)
-    if options.mirs:
-        mirs = get_mir_counts(bamfile, options.gff_file, options.quality_threshold)
-        write_counts(mirs, options.output_mature_mirs)
 
-if __main__():
-    __main__()
+def main():
+    args = Parser()
+    bamfile = pysam.AlignmentFile(args.alignment_file, 'rb', check_sq=False)
+    if args.pre_mirs:
+        pre_mirs = get_pre_mir_counts(bamfile)
+        write_counts(pre_mirs, args.pre_mirs)
+        if args.lattice:
+            pre_mirs_coverage = get_pre_mir_coverage(bamfile,
+                                                     args.quality_threshold)
+            write_dataframe_coverage(pre_mirs_coverage, args.lattice)
+    if args.mirs:
+        mirs = get_mir_counts(bamfile, args.gff_file)
+        write_counts(mirs, args.mirs)
 
+
+if __name__ == '__main__':
+    main()
