01f52eecaeac8a4950936a7cde1ca452e66e77c8
drosofff <drosofff@gmail.com>
2017-07-21 16:25:40 +0200

    big OO refactoring of small_rna_map.py

--- tools/small_rna_map/small_rna_map.py
+++ tools/small_rna_map/small_rna_map.py
@@ -18,48 +18,18 @@ def Parser():
     return args
 
 
-def compute_map_dictionary(pysam_object):
-    '''
-    returns
-    - map_dictionary {(read_chromosome,read_position,polarity):
-                     [list_of_read_length]}
-    - max_dictionary {chromosome_name:
-                      max_of_number_of_read_at_any_position}
-    '''
-    unmatched_chromosomes = list(pysam_object.references)
-    map_dictionary = defaultdict(list)
-    max_dictionary = dict.fromkeys(pysam_object.references, 0)
-    for read in pysam_object:
-        if not read.is_unmapped:
-            read_chromosome = read.reference_name
-            # to shift from 0-based to 1-based coordinate
-            read_position = read.pos + 1
-            polarity = "F" if (not read.is_reverse) else "R"
-            map_dictionary[(read_chromosome, read_position,
-                            polarity)].append(read.qlen)
-            if read_chromosome in unmatched_chromosomes:
-                unmatched_chromosomes.remove(read_chromosome)
-    for name in unmatched_chromosomes:
-        # to put at least a null value by chromosome
-        map_dictionary[(name, 1, 'F')] = [0]
-    # make a dictionary with max value of reads by chromosome
-    max_dictionary = defaultdict(int)
-    for name in pysam_object.references:
-        for key in map_dictionary.keys():
-            if name in key and len(map_dictionary[key]) > max_dictionary[name]:
-                max_dictionary[name] = len(map_dictionary[key])
-    return map_dictionary, max_dictionary
-
-
 class Map:
 
     def __init__(self, bam_file, sample):
         self.sample_name = sample
         self.bam_object = pysam.AlignmentFile(bam_file, 'rb')
-        self.chromosomes = self.bam_object.references
+        self.chromosomes = dict(zip(self.bam_object.references,
+                                self.bam_object.lengths))
         self.map_dict = self.create_map(self.bam_object)
-        self.absent_chromosomes = self.get_absent_chrom(self.map_dict)
         self.max = self.compute_max(self.map_dict)
+        self.mean = self.compute_mean(self.map_dict)
+        self.median = self.compute_median(self.map_dict)
+        self.coverage = self.compute_coverage(self.map_dict) 
 
     def create_map(self, bam_object):
         '''
@@ -67,8 +37,12 @@ class Map:
                                                        [read_length, ...]}
         alternate method without empty chromosomes
         '''
-        map_dictionary = defaultdict(list)
-        for chrom in bam_object.references:
+        map_dictionary = dict()
+        for chrom in self.chromosomes:
+            for pos in range (self.chromosomes[chrom]):
+                map_dictionary[(chrom, pos+1, 'F')] = []  # get all chromosomes
+                map_dictionary[(chrom, pos+1, 'R')] = []  # get all chromosomes
+        for chrom in self.chromosomes:
             for read in bam_object.fetch(chrom):
                 read_positions = read.positions  # a list of covered positions
                 if read.is_reverse:
@@ -99,10 +73,13 @@ class Map:
                                                        mean_value_of_reads}
         '''
         mean_dictionary = dict()
-        for key in mean_dictionary:
-            mean_dictionary = mean(mean_dictionary[key])
+        for key in map_dictionary:
+            if len(map_dictionary[key]) == 0:
+                mean_dictionary[key] = 0
+            else:
+                mean_dictionary[key] = round(numpy.mean(map_dictionary[key]),
+                                                                            1)
         return mean_dictionary
-
 
     def compute_median(self, map_dictionary):
         '''
@@ -111,29 +88,56 @@ class Map:
                                                        mean_value_of_reads}
         '''
         median_dictionary = dict()
-        for key in median_dictionary:
-            median_dictionary = median(median_dictionary[key])
+        for key in map_dictionary:
+            if len(map_dictionary[key]) == 0:
+                median_dictionary[key] = 0
+            else:
+                median_dictionary[key] = numpy.median(map_dictionary[key])
         return median_dictionary
 
+    def compute_coverage(self, map_dictionary, quality=10):
+        """
+        Takes a AlignmentFile object and returns a dictionary of lists
+        of coverage along the coordinates of pre_mirs (as keys)
+        """
+        coverage_dictionary = dict()
+        for chrom in self.chromosomes:
+            coverage = self.bam_object.count_coverage(reference=chrom,
+                                                start=0,
+                                                end=self.chromosomes[chrom],
+                                                quality_threshold=quality)
+            """ Add the 4 coverage values """
+            coverage = [sum(x) for x in zip(*coverage)]
+            for pos, cov in enumerate(coverage):
+                coverage_dictionary[(chrom, pos+1, "F")] = cov
+                coverage_dictionary[(chrom, pos+1, "R")] = cov
+        return coverage_dictionary
 
-def main(inputs, sample_names, output):
-    with open(output, 'w') as outfile:
-        outfile.write('\t'.join(['Dataset', 'Chromosome', 'Chrom_length',
-                                 'Coordinate', 'Nbr_reads', 'Polarity',
-                                 'Max', 'Mean', 'Median\n']))
-        chr_lengths = dict(zip(pysam.AlignmentFile(inputs[0], 'rb').references,
-                               pysam.AlignmentFile(inputs[0], 'rb').lengths))
-        for file, sample in zip(inputs, sample_names):
-            with pysam.AlignmentFile(file, 'rb') as bamfile:
-                map, maximum = compute_map_dictionary(bamfile)
-                for record in sorted(map):
-                    line = [sample, record[0],
-                            str(chr_lengths[record[0]]),
-                            str(record[1]), str(len(map[record])),
-                            record[2], str(maximum[record[0]]),
-                            str(round(numpy.mean(map[record]), 1)),
-                            str(numpy.median(map[record]))]
-                    outfile.write("\t".join(line) + "\n")
+    def write_table(self, out):
+        '''
+        Dataset, Chromosome, Chrom_length, Coordinate, Nbr_reads
+        Polarity, Max, Mean, Median, Coverage
+        out is an *open* file handler
+        '''
+        for key in sorted(self.map_dict):
+            line = [self.sample_name, key[0], self.chromosomes[key[0]],
+                    key[1], len(self.map_dict[key]), key[2], self.max[key[0]],
+                    self.mean[key], self.median[key], self.coverage[key]]
+            line = [str(i) for i in line]
+            out.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % tuple(line))
+
+
+def main(inputs, samples, file_out):
+    F = open(file_out, 'w')
+    header = tuple(["Dataset", "Chromosome", "Chrom_length", "Coordinate",
+                    "Nbr_reads", "Polarity", "Max", "Mean", "Median",
+                    "Coverage"])
+    header = '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % header
+    F.write(header)
+    for file, sample in zip(inputs, samples):
+        mapobj = Map(file, sample)
+        mapobj.write_table(F)
+    F.close()
 
 
 if __name__ == "__main__":
