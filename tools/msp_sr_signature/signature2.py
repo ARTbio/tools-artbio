961d6cce6e903edf44941a8591ae7682c1e10d6f
drosofff <drosofff@gmail.com>
2017-08-25 18:19:33 +0200

    python code almost finished. must work out R script now. tests must still fail

--- tools/msp_sr_signature/signature2.py
+++ tools/msp_sr_signature/signature2.py
@@ -5,6 +5,7 @@ import numpy
 
 import pysam
 
+
 def Parser():
     the_parser = argparse.ArgumentParser()
     the_parser.add_argument(
@@ -35,10 +36,11 @@ def Parser():
         help="z-signature dataframe")
     the_parser.add_argument(
         '--graph', action='store', choices=["global", "by-item"],
-        help="small RNA signature is computed either globally or\
-        by item (global-lattice)")
+        help="small RNA signature is computed either globally or \
+              by item (global-lattice)")
     args = the_parser.parse_args()
     return args
+
 
 class Map:
 
@@ -70,8 +72,8 @@ class Map:
         return map_dictionary
 
     def signature_tables(self, minquery, maxquery, mintarget, maxtarget):
-        query_range = range (minquery, maxquery+1)
-        target_range = range (mintarget, maxtarget+1)
+        query_range = range(minquery, maxquery + 1)
+        target_range = range(mintarget, maxtarget + 1)
         Query_table = defaultdict(dict)
         Target_table = defaultdict(dict)
         for key in self.map_dict:
@@ -85,13 +87,13 @@ class Map:
                     Query_table[key[0]][coordinate] = Query_table[key[0]].get(
                         coordinate, 0) + 1
                 if size in target_range:
-                    Target_table[key[0]][coordinate] = Target_table[key[0]].get(
-                        coordinate, 0) + 1
+                    Target_table[key[0]][coordinate] = \
+                        Target_table[key[0]].get(coordinate, 0) + 1
         return Query_table, Target_table
 
     def compute_signature_z(self, minquery, maxquery, mintarget, maxtarget,
-                            scope, genome_wide=False, zscore="no"):
-        Query_table, Target_table = signature_tables(minquery, maxquery,
+                            scope, zscore="no"):
+        Query_table, Target_table = self.signature_tables(minquery, maxquery,
                                                      mintarget, maxtarget)
         frequency_table = defaultdict(dict)
         for chrom in self.chromosomes:
@@ -102,38 +104,24 @@ class Map:
                 for overlap in scope:
                     frequency_table[chrom][overlap] += min(
                         Query_table[chrom][coord],
-                        Target_table[chrom].get(-coord -i +1, 0))
+                        Target_table[chrom].get(-coord - overlap + 1, 0))
         # since we want the number of pairs, not the number or paired reads
+        #### WORK AT THIS, DO NOT LEAVE IT AS IS ####
         for chrom in frequency_table:
             for overlap in frequency_table[chrom]:
                 frequency_table[chrom][overlap] /= 2
-        # collapse if genome_wide = True
+        # compute overlaps for all chromosomes merged
         for overlap in scope:
             accumulator = []
             for chrom in frequency_table:
-                accumulator.append(frequency_table[chrom][overlap])
+                if chrom != 'all_chromosomes':
+                    accumulator.append(frequency_table[chrom][overlap])
             frequency_table['all_chromosomes'][overlap] = sum(accumulator)
-        # compute z-scores by chromosomes and for all-chromosomes
-        for chrom in frequency_table:
-            accumulator = []
-            for overlap in frequency_table[chrom]:
-                accumulator.append(frequency_table[chrom][overlap])
-            z_mean = mean(accumulator)
-            z_std = std(accumulator)
-            if z_std == 0:
-                for overlap in frequency_table[chrom]:
-                    frequency_table[chrom][overlap] =
-                        [frequency_table[chrom][overlap], 0]
-            else:
-                for overlap in frequency_table[chrom]:
-                    frequency_table[chrom][overlap] =
-                        [frequency_table[chrom][overlap],
-                        (frequency_table[chrom][overlap]- z_mean)/z_std]
-        return frequency_table
+        return self.stringify_table(frequency_table)
 
-    def compute_signature_h_byitem(self, minquery, maxquery, mintarget,
-                                   maxtarget, scope, genome_wide=False):
-        Query_table, Target_table = signature_tables(minquery, maxquery,
+    def compute_signature_h(self, minquery, maxquery, mintarget,
+                                   maxtarget, scope):
+        Query_table, Target_table = self.signature_tables(minquery, maxquery,
                                                      mintarget, maxtarget)
         frequency_table = defaultdict(dict)
         for chrom in self.chromosomes:
@@ -143,33 +131,78 @@ class Map:
             Total_Query_Numb = 0
             for coord in Query_table[chrom]:
                 Total_Query_Numb += Query_table[chrom][coord]
-            for coord in Query_table[chrom]:
-                local_table = dict ([(overlap, 0) for overlap in scope])
+                local_table = dict([(overlap, 0) for overlap in scope])
                 number_of_targets = 0
                 for overlap in scope:
-                    local_table[overlap] += Query_table[chrom][coord] *
-                        Target_table[chrom].get(-offset -i +1, 0)
-                    number_of_targets +=
-                        Target_table[chrom].get(-offset -i +1, 0)
+                    local_table[overlap] += Query_table[chrom][coord] * \
+                        Target_table[chrom].get(-coord - overlap + 1, 0)
+                    number_of_targets += Target_table[chrom].get(
+                        -coord - overlap + 1, 0)
                 for overlap in scope:
                     try:
-                        frequency_table[overlap] += (1. / number_of_targets
-                            / Total_Query_Numb) * frequency_table[i]
-                    except ZeroDivisionError :
+                        frequency_table[chrom][overlap] += \
+                            local_table[overlap] / number_of_targets \
+                            / float(Total_Query_Numb)
+                    except ZeroDivisionError:
                         continue
-        return general_frequency_table      
+        # compute overlap probabilities for all chromosomes merged
+        general_frequency_table = dict([(overlap, 0) for overlap in scope])
+        total_aligned_reads = self.bam_object.count()
+        for chrom in frequency_table:
+            for overlap in frequency_table[chrom]:
+                try:
+                    general_frequency_table[overlap] += \
+                        frequency_table[chrom][overlap] / total_aligned_reads \
+                        * self.bam_object.count(chrom)
+                except ZeroDivisionError:
+                    continue
+        for overlap in general_frequency_table:
+            frequency_table['all_chromosomes'][overlap] = \
+                general_frequency_table[overlap]
+        return self.stringify_table(frequency_table)
 
-    def format_table(self, table):
-    
+    def stringify_table(self, frequency_table):
+        '''
+        method both to compute z-score and to return a writable string
+        '''
+        tablestring = []
+        for chrom in sorted(frequency_table):
+            accumulator = []
+            for overlap in frequency_table[chrom]:
+                accumulator.append(frequency_table[chrom][overlap])
+            z_mean = numpy.mean(accumulator)
+            z_std = numpy.std(accumulator)
+            if z_std == 0:
+                for overlap in sorted(frequency_table[chrom]):
+                    tablestring.append('%s\t%s\t%s\t%s\n' % (
+                        chrom, str(overlap),
+                        str(frequency_table[chrom][overlap]), str(0)))
+            else:
+                for overlap in sorted(frequency_table[chrom]):
+                    tablestring.append('%s\t%s\t%s\t%s\n' % (
+                        chrom, str(overlap),
+                        str(frequency_table[chrom][overlap]),
+                        str((frequency_table[chrom][overlap] - z_mean)/z_std)))
+        return ''.join(tablestring)
 
-def main(input):
-    F = open(output, 'w')
-    I = open(input, 'read')
+
+
+def main(input, minquery, maxquery, mintarget, maxtarget, minscope, maxscope,
+         output_h, output_z, genome_wide=False, zscore="no"):
+    H = open(output_h, 'w')
+    Z = open(output_z, 'w')
     mapobj = Map(input)
-    something = mapobj.compute_signature_z(minquery, maxquery, mintarget,
-                    maxtarget, scope, genome_wide=False, zscore="no")
+    scope = range(minscope, maxscope + 1)
+    Z.write(mapobj.compute_signature_z(minquery, maxquery, mintarget,
+            maxtarget, scope, zscore="no"))
+    H.write(mapobj.compute_signature_h(minquery, maxquery, mintarget,
+            maxtarget, scope))
+    H.close()
+    Z.close()
 
 
 if __name__ == "__main__":
     args = Parser()
-    main(args., args., args., args.)
+    main(args.input, args.minquery, args.maxquery, args.mintarget,
+         args.maxtarget, args.minscope, args.maxscope, args.output_h,
+         args.output_z)
