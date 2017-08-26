943f4f25515e962f1fd57c8e98108c4cc28ee7f3
drosofff <drosofff@gmail.com>
2017-08-26 14:09:46 +0200

    global signature almost finalized - test to be updated

--- tools/msp_sr_signature/signature.py
+++ tools/msp_sr_signature/signature.py
@@ -1,120 +1,207 @@
-#!/usr/bin/python
-# script for computing overlap signatures from a bowtie output
-# Christophe Antoniewski <drosofff@gmail.com>
-# Usage signature.py <1:input> <2:format of input> <3:minsize query> <4:maxsize query> <5:minsize target> <6:maxsize target>
-# <7:minscope> <8:maxscope> <9:output> <10:bowtie index> <11:procedure option> <12: graph (global or lattice)>
-# <13: R code>
-# version 2.0.0
-
-import subprocess
 import argparse
-from smRtools import HandleSmRNAwindows
+from collections import defaultdict
+
+import numpy
+
+import pysam
 
 
 def Parser():
     the_parser = argparse.ArgumentParser()
     the_parser.add_argument(
-        '--input', action="store", type=str, help="input alignment file")
-    the_parser.add_argument('--inputFormat', action="store", type=str, choices=[
-                            "tabular", "bam", "sam"], help="format of alignment file (tabular/bam/sam)")
+        '--input', action="store", type=str, help="bam alignment file")
     the_parser.add_argument(
-        '--minquery', type=int, help="Minimum readsize of query reads (nt) - must be an integer")
+        '--minquery', type=int,
+        help="Minimum readsize of query reads (nt) - must be an integer")
     the_parser.add_argument(
-        '--maxquery', type=int, help="Maximum readsize of query reads (nt) - must be an integer")
+        '--maxquery', type=int,
+        help="Maximum readsize of query reads (nt) - must be an integer")
     the_parser.add_argument(
-        '--mintarget', type=int, help="Minimum readsize of target reads (nt) - must be an integer")
+        '--mintarget', type=int,
+        help="Minimum readsize of target reads (nt) - must be an integer")
     the_parser.add_argument(
-        '--maxtarget', type=int, help="Maximum readsize of target reads (nt) - must be an integer")
+        '--maxtarget', type=int,
+        help="Maximum readsize of target reads (nt) - must be an integer")
     the_parser.add_argument(
-        '--minscope', type=int, help="Minimum overlap analyzed (nt) - must be an integer")
+        '--minscope', type=int,
+        help="Minimum overlap analyzed (nt) - must be an integer")
     the_parser.add_argument(
-        '--maxscope', type=int, help="Maximum overlap analyzed (nt) - must be an integer")
+        '--maxscope', type=int,
+        help="Maximum overlap analyzed (nt) - must be an integer")
     the_parser.add_argument(
-        '--outputOverlapDataframe', action="store", type=str, help="Overlap dataframe")
-    the_parser.add_argument('--referenceGenome', action='store',
-                            help="path to the bowtie-indexed or fasta reference")
-    the_parser.add_argument('--extract_index', action='store_true',
-                            help="specify if the reference is an indexed Bowtie reference")
-    the_parser.add_argument('--graph', action='store', choices=[
-                            "global", "lattice"], help="small RNA signature is computed either globally or by item (global-lattice)")
+        '--output_h', action="store", type=str,
+        help="h-signature dataframe")
     the_parser.add_argument(
-        '--rcode', type=str, help="R code to be passed to the python script")
+        '--output_z', action="store", type=str,
+        help="z-signature dataframe")
     args = the_parser.parse_args()
     return args
 
-args = Parser()
 
-if args.extract_index:
-    GenomeFormat = "bowtieIndex"
-else:
-    GenomeFormat = "fastaSource"
+class Map:
 
-if args.inputFormat == "tabular":
-    Genome = HandleSmRNAwindows(
-        args.input, args.inputFormat, args.referenceGenome, GenomeFormat)
-elif args.inputFormat == "sam":
-    Genome = HandleSmRNAwindows(
-        args.input, args.inputFormat, args.referenceGenome, GenomeFormat)
-else:
-    Genome = HandleSmRNAwindows(
-        args.input, args.inputFormat, args.referenceGenome, GenomeFormat)
+    def __init__(self, bam_file):
+        self.bam_object = pysam.AlignmentFile(bam_file, 'rb')
+        self.chromosomes = dict(zip(self.bam_object.references,
+                                self.bam_object.lengths))
+        self.map_dict = self.create_map(self.bam_object)
 
-# replace objDic by Genome.instanceDict or... objDic = Genome.instanceDict
-objDic = Genome.instanceDict
+    def create_map(self, bam_object):
+        '''
+        Returns a map_dictionary {(chromosome,read_position,polarity):
+                                                    [read_length, ...]}
+        '''
+        map_dictionary = defaultdict(list)
+        # get empty value for start and end of each chromosome
+        for chrom in self.chromosomes:
+            map_dictionary[(chrom, 1, 'F')] = []
+            map_dictionary[(chrom, self.chromosomes[chrom], 'F')] = []
+        for chrom in self.chromosomes:
+            for read in bam_object.fetch(chrom):
+                positions = read.positions  # a list of covered positions
+                if read.is_reverse:
+                    map_dictionary[(chrom, positions[-1]+1,
+                                    'R')].append(read.query_alignment_length)
+                else:
+                    map_dictionary[(chrom, positions[0]+1,
+                                    'F')].append(read.query_alignment_length)
+        return map_dictionary
 
-args.maxscope += 1
+    def signature_tables(self, minquery, maxquery, mintarget, maxtarget):
+        query_range = range(minquery, maxquery + 1)
+        target_range = range(mintarget, maxtarget + 1)
+        Query_table = defaultdict(dict)
+        Target_table = defaultdict(dict)
+        for key in self.map_dict:
+            for size in self.map_dict[key]:
+                if size in query_range or size in target_range:
+                    if key[2] == 'F':
+                        coordinate = key[1]
+                    else:
+                        coordinate = -key[1]
+                if size in query_range:
+                    Query_table[key[0]][coordinate] = Query_table[key[0]].get(
+                        coordinate, 0) + 1
+                if size in target_range:
+                    Target_table[key[0]][coordinate] = \
+                        Target_table[key[0]].get(coordinate, 0) + 1
+        return Query_table, Target_table
 
-general_frequency_table = dict(
-    [(i, 0) for i in range(args.minscope, args.maxscope)])
-general_percent_table = dict(
-    [(i, 0) for i in range(args.minscope, args.maxscope)])
-OUT = open(args.outputOverlapDataframe, "w")
+    def compute_signature_z(self, minquery, maxquery, mintarget, maxtarget,
+                            scope, zscore="no"):
+        Query_table, Target_table = self.signature_tables(minquery, maxquery,
+                                                     mintarget, maxtarget)
+        frequency_table = defaultdict(dict)
+        for chrom in self.chromosomes:
+            for overlap in scope:
+                frequency_table[chrom][overlap] = 0
+        for chrom in Query_table:
+            for coord in Query_table[chrom]:
+                for overlap in scope:
+                    frequency_table[chrom][overlap] += min(
+                        Query_table[chrom][coord],
+                        Target_table[chrom].get(-coord - overlap + 1, 0))
+        # since we want the number of pairs, not the number or paired reads
+        #### WORK AT THIS, DO NOT LEAVE IT AS IS ####
+        for chrom in frequency_table:
+            for overlap in frequency_table[chrom]:
+                frequency_table[chrom][overlap] /= 2
+        # compute overlaps for all chromosomes merged
+        for overlap in scope:
+            accumulator = []
+            for chrom in frequency_table:
+                if chrom != 'all_chromosomes':
+                    accumulator.append(frequency_table[chrom][overlap])
+            frequency_table['all_chromosomes'][overlap] = sum(accumulator)
+        return self.stringify_table(frequency_table)
 
-if args.graph == "global":
-    # for normalized summing of local_percent_table(s)
-    readcount_dic = {}
-    Total_read_in_objDic = 0
-    for item in objDic:
-        readcount_dic[item] = objDic[item].readcount(
-            args.minquery, args.maxquery)
-        Total_read_in_objDic += readcount_dic[item]
-    ######
-    for x in (objDic):
-        local_frequency_table = objDic[x].signature(
-            args.minquery, args.maxquery, args.mintarget, args.maxtarget, range(args.minscope, args.maxscope))
-        local_percent_table = objDic[x].hannon_signature(
-            args.minquery, args.maxquery, args.mintarget, args.maxtarget, range(args.minscope, args.maxscope))
-        try:
-            for overlap in local_frequency_table.keys():
-                general_frequency_table[overlap] = general_frequency_table.get(
-                    overlap, 0) + local_frequency_table[overlap]
-        except:
-            pass
-        try:
-            for overlap in local_percent_table.keys():
-                general_percent_table[overlap] = general_percent_table.get(
-                    overlap, 0) + (1. / Total_read_in_objDic * readcount_dic[x] * local_percent_table[overlap])
-        except:
-            pass
-    print >> OUT, "overlap\tnum of pairs\tprobability"
-    for classe in sorted(general_frequency_table):
-        print >> OUT, "%i\t%i\t%f" % (
-            classe, general_frequency_table[classe], general_percent_table[classe])
+    def compute_signature_h(self, minquery, maxquery, mintarget,
+                                   maxtarget, scope):
+        Query_table, Target_table = self.signature_tables(minquery, maxquery,
+                                                     mintarget, maxtarget)
+        frequency_table = defaultdict(dict)
+        for chrom in self.chromosomes:
+            for overlap in scope:
+                frequency_table[chrom][overlap] = 0
+        for chrom in Query_table:
+            Total_Query_Numb = 0
+            for coord in Query_table[chrom]:
+                Total_Query_Numb += Query_table[chrom][coord]
+                local_table = dict([(overlap, 0) for overlap in scope])
+                number_of_targets = 0
+                for overlap in scope:
+                    local_table[overlap] += Query_table[chrom][coord] * \
+                        Target_table[chrom].get(-coord - overlap + 1, 0)
+                    number_of_targets += Target_table[chrom].get(
+                        -coord - overlap + 1, 0)
+                for overlap in scope:
+                    try:
+                        frequency_table[chrom][overlap] += \
+                            local_table[overlap] / number_of_targets \
+                            / float(Total_Query_Numb)
+                    except ZeroDivisionError:
+                        continue
+        # compute overlap probabilities for all chromosomes merged
+        general_frequency_table = dict([(overlap, 0) for overlap in scope])
+        total_aligned_reads = 0
+        for chrom in frequency_table:
+            for overlap in frequency_table[chrom]:
+                total_aligned_reads += self.bam_object.count(chrom)
+        for chrom in frequency_table:
+            for overlap in frequency_table[chrom]:
+                try:
+                    general_frequency_table[overlap] += \
+                        frequency_table[chrom][overlap] / total_aligned_reads \
+                        * self.bam_object.count(chrom)
+                except ZeroDivisionError:
+                    continue
+        for overlap in general_frequency_table:
+            frequency_table['all_chromosomes'][overlap] = \
+                general_frequency_table[overlap]
+        return self.stringify_table(frequency_table)
 
-else:
-    print >> OUT, "overlap\tnum of pairs\tprobability\titem"
-    for x in (objDic):
-        local_frequency_table = objDic[x].signature(
-            args.minquery, args.maxquery, args.mintarget, args.maxtarget, range(args.minscope, args.maxscope))
-        local_percent_table = objDic[x].hannon_signature(
-            args.minquery, args.maxquery, args.mintarget, args.maxtarget, range(args.minscope, args.maxscope))
-        for classe in range(args.minscope, args.maxscope):
-            print >> OUT, "%i\t%i\t%f\t%s" % (
-                classe, local_frequency_table[classe], local_percent_table[classe], x)
+    def stringify_table(self, frequency_table):
+        '''
+        method both to compute z-score and to return a writable string
+        '''
+        tablestring = []
+        for chrom in sorted(frequency_table):
+            accumulator = []
+            for overlap in frequency_table[chrom]:
+                accumulator.append(frequency_table[chrom][overlap])
+            z_mean = numpy.mean(accumulator)
+            z_std = numpy.std(accumulator)
+            if z_std == 0:
+                for overlap in sorted(frequency_table[chrom]):
+                    tablestring.append('%s\t%s\t%s\t%s\n' % (
+                        chrom, str(overlap),
+                        str(frequency_table[chrom][overlap]), str(0)))
+            else:
+                for overlap in sorted(frequency_table[chrom]):
+                    tablestring.append('%s\t%s\t%s\t%s\n' % (
+                        chrom, str(overlap),
+                        str(frequency_table[chrom][overlap]),
+                        str((frequency_table[chrom][overlap] - z_mean)/z_std)))
+        return ''.join(tablestring)
 
-OUT.close()
 
-# Run the R script that is defined in the xml using the Rscript binary
-# provided with R.
-R_command = "Rscript " + args.rcode
-process = subprocess.Popen(R_command.split())
+
+def main(input, minquery, maxquery, mintarget, maxtarget, minscope, maxscope,
+         output_h, output_z, genome_wide=False, zscore="no"):
+    H = open(output_h, 'w')
+    Z = open(output_z, 'w')
+    mapobj = Map(input)
+    scope = range(minscope, maxscope + 1)
+    Z.write(mapobj.compute_signature_z(minquery, maxquery, mintarget,
+            maxtarget, scope, zscore="no"))
+    H.write(mapobj.compute_signature_h(minquery, maxquery, mintarget,
+            maxtarget, scope))
+    H.close()
+    Z.close()
+
+
+if __name__ == "__main__":
+    args = Parser()
+    main(args.input, args.minquery, args.maxquery, args.mintarget,
+         args.maxtarget, args.minscope, args.maxscope, args.output_h,
+         args.output_z)
--- tools/msp_sr_signature/signature.r
+++ tools/msp_sr_signature/signature.r
@@ -1,56 +1,78 @@
-      graph_type = "${graph_type}"
+## Setup R error handling to go to stderr
+options( show.error.messages=F,
+       error = function () { cat( geterrmessage(), file=stderr() ); q( "no", 1, F ) } )
+warnings()
 
+library(RColorBrewer)
+library(lattice)
+library(latticeExtra)
+library(grid)
+library(gridExtra)
+library(optparse)
+
+option_list <- list(
+    make_option("--h_dataframe", type="character", help="path to h-signature dataframe"),
+    make_option("--z_dataframe", type="character", help="path to z-signature dataframe"),
+    make_option("--plot_method", type = "character", help="How  data should be plotted (global of by-item)"),
+    make_option("--pdf", type = "character", help="path to the pdf file with plots"),
+    make_option("--title", type = "character", help="Graph Title")
+    )
+ 
+parser <- OptionParser(usage = "%prog [options] file", option_list = option_list)
+args = parse_args(parser)
+
+
+
+ 
+# data frames implementation
+h_dataframe = read.delim(args$h_dataframe, header=F)
+colnames(h_dataframe) = c("chrom", "overlap", "prob", "z-score")
+h_dataframe$prob = h_dataframe$prob * 100  # to get probs in %
+z_dataframe = read.delim(args$z_dataframe, header=F)
+colnames(z_dataframe) = c("chrom", "overlap", "nbre_pairs", "z-score")
+
+# functions
       globalgraph = function () {
-        ## Setup R error handling to go to stderr
-        options( show.error.messages=F,
-                 error = function () { cat( geterrmessage(), file=stderr() ); q( "no", 1, F ) } )
-        signature = read.delim("${output}", header=TRUE)
-        signaturez=data.frame(signature[,1], (signature[,2] -mean(signature[,2]))/sd(signature[,2]))
-        overlap_prob_z=data.frame(signature[,1], (signature[,3] -mean(signature[,3]))/sd(signature[,3]))
-        YLIM=max(signature[,2])
+        pdf(args$pdf)
+        par(mfrow=c(2,2),oma = c(0, 0, 3, 0))
         
+        plot(z_dataframe[z_dataframe$chrom == "all_chromosomes", c(2,3)],
+             type = "h", main="Numbers of pairs", cex.main=1, xlab="overlap (nt)",
+             ylab="Numbers of pairs", col="darkslateblue", lwd=4)
 
-        ## Open output2 PDF file
-        pdf( "${output2}" )
-        if (YLIM!=0) {
-          par(mfrow=c(2,2),oma = c(0, 0, 3, 0))
+        plot(z_dataframe[z_dataframe$chrom == "all_chromosomes", c(2,4)],
+             type = "l", main="Number of pairs Z-scores", cex.main=1, xlab="overlap (nt)",
+             ylab="z-score", pch=19, cex=0.2, col="darkslateblue", lwd=2)
 
-          plot(signature[,1:2], type = "h", main="Numbers of pairs", cex.main=1, xlab="overlap (nt)", ylim=c(0,YLIM), ylab="Numbers of pairs", col="darkslateblue", lwd=4)
+        plot(h_dataframe[h_dataframe$chrom == "all_chromosomes", c(2,3)],
+             type = "l", main="Overlap probabilities", cex.main=1, xlab="overlap (nt)",
+             ylab="Probability [%]", ylim=c(0,50), pch=19, col="darkslateblue", lwd=2)
 
-          plot(signaturez, type = "l", main="Number of pairs Z-scores", cex.main=1, xlab="overlap (nt)", ylab="z-score", pch=19, cex=0.2, col="darkslateblue", lwd=2)
+        plot(h_dataframe[h_dataframe$chrom == "all_chromosomes", c(2,4)],
+             type = "l", main="Overlap Probability Z-scores", cex.main=1,
+             xlab="overlap (nt)", ylab="z-score", pch=19, cex=0.2,
+             col="darkslateblue", lwd=2)
 
-          plot(signature[,1], signature[,3]*100, type = "l", main="Overlap probabilities",
-             cex.main=1, xlab="overlap (nt)", ylab="Probability [%]", ylim=c(0,50),
-             pch=19, col="darkslateblue", lwd=2)
-
-          plot(overlap_prob_z, type = "l", main="Overlap Probability Z-scores", cex.main=1, xlab="overlap (nt)", ylab="z-score", pch=19, cex=0.2, col="darkslateblue", lwd=2)
-
-          mtext("Overlap Signatures of ${minquery}-${maxquery} against ${mintarget}-${maxtarget}nt small RNAs", outer = TRUE, cex=1)
-        }
-        devname = dev.off()
-        ## Close the PDF file
+        mtext(args$title, outer = TRUE, cex=1)
+        dev.off()
       }
 
       treillisgraph = function () {
-        ## Open output2 PDF file
-        pdf( "${output2}", paper="special", height=11.69, width=8.2677 )
-        signature = read.delim("${output}", header=TRUE)
-        options( show.error.messages=F,
-               error = function () { cat( geterrmessage(), file=stderr() ); q( "no", 1, F ) } )
-        library(lattice)
-        print (xyplot(signature[,3]*100~signature[,1]|signature[,4], type = "l", xlim=c(${minscope},${maxscope}), main="ping-pong Signature of ${minquery}-${maxquery} against ${mintarget}-${maxtarget}nt small RNAs",
-             par.strip.text=list(cex=.5), strip=strip.custom(which.given=1, bg="lightblue"), scales=list(cex=0.5),
-             cex.main=1, cex=.5, xlab="overlap (nt)", ylab="ping-pong signal [%]",
-             pch=19, col="darkslateblue", lwd =1.5, cex.lab=1.2, cex.axis=1.2,
-             layout=c(4,12), as.table=TRUE, newpage = T) )
-        devnname = dev.off()
-      }
+        pdf( args$pdf, paper="special", height=11.69, width=8.2677 )
+#        signature = read.delim("${output}", header=TRUE)
+#        xyplot(signature[,3]*100~signature[,1]|signature[,4], type = "l", xlim=c(${minscope},${maxscope}), main="ping-pong Signature of ${minquery}-${maxquery} against ${mintarget}-${maxtarget}nt small RNAs",
+#             par.strip.text=list(cex=.5), strip=strip.custom(which.given=1, bg="lightblue"), scales=list(cex=0.5),
+#             cex.main=1, cex=.5, xlab="overlap (nt)", ylab="ping-pong signal [%]",
+#             pch=19, col="darkslateblue", lwd =1.5, cex.lab=1.2, cex.axis=1.2,
+#             layout=c(4,12), as.table=TRUE, newpage = T)
+#        dev.off()
+#      }
 
-      if (graph_type=="global") {
+      if (args$plot_method=="global") {
         globalgraph()
 
       }
-      if(graph_type=="lattice") {
+      if(args$plot_method=="by-item") {
         treillisgraph()
       }
 
