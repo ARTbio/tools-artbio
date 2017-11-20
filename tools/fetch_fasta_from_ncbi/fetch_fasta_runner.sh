efe766601b424ee0097aebabad8520a91d0dc6b8
ccedmendoza <cedricmendoza@gmail.com>
2017-11-20 11:14:53 +0100

    couldn't do it in wrapper so a different script will run fetch_fasta

---
+++ tools/fetch_fasta_from_ncbi/fetch_fasta_runner.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+
+getParams() {
+  # Get parameters
+  tool_path=$2
+  queryString=$4
+  dbname=$6
+  logfile=$8
+  outfile=$11
+  params=(tool_path queryString dbname logfile outfile)
+  return params
+}
+
+params=getParams();
+echo $params;
