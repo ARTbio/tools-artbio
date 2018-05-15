8877430afcf674771494496b8eba2dc387e46724
drosofff <drosofff@gmail.com>
2018-05-15 18:14:24 +0200

    replace labels=None by labels='' because None cannot be serialized

--- helper_scripts/pack_shed_tool_conf.py
+++ helper_scripts/pack_shed_tool_conf.py
@@ -33,7 +33,7 @@ def merge_xml_sections(input_file, output_file):
                     if 'labels' in item.attrib:
                         labels = item.attrib['labels']
                     else:
-                        labels = None
+                        labels = ""
                     tool = ET.SubElement(child, "tool",
                                          file=item.attrib['file'],
                                          guid=item.attrib['guid'],
