c784d67ec5dc634221b564070d64ceb16f51f459
Naira <32098160+NairaNaouar@users.noreply.github.com>
2021-03-08 00:22:23 +0100

    Scripts to generate HRD input files [WIP] (#507)
    * Scripts to generate HRD input file
    
    * change tool name and add a .shed.yml to trigger tests
    
    * add macro, xml and test-data
    
    * add vcf sanitizer
    
    * fix sanitize_snv.py
    
    * not too buggy draft with test
    
    * remove locale setting in R scripts
    
    * final working version. must lint R
    
    * lint R code and try fixing TZ tidiverse bug
    
    * test test bugs...
    
    * fix bugs in sequenza_to_hrdtools_input.R
    
    * lot of tests to highlight chromosome sorting in reference
    
    * fix R synthax for linting
    
    * copy reference in working dir for high parallel
    
    * separate indexing and vcf calling
    
    * comment error management code
    
    * install tzdata in test instance
    
    * try to set time zone to UTC
    
    * lint segmentation_sequenza.R
    
    * try to set time zone to Utc/UTC
    
    * specify zdir in R
    
    * add consistent help section and reference
    
    * turn off R warnings
    
    * shift from write_tsv to write.table
    
    * lint R code
    
    * work on sequenza_extract
    
    * change conda environment
    
    * add explicitely libgfortran to conda env
    
    * fix libgfortran reference
    
    * revert to head environment and add tzdata
    
    * include library readr and call to locale to fix olso database
    
    * set env var `TZ` to Europe/Paris
    
    * add Sys.timezone(location = TRUE)
    
    * more test on Olson base
    
    * re add tzdata in the test container
    
    * specify hard path for timezone
    
    Co-authored-by: drosofff <drosofff@gmail.com>

---
+++ tools/snvtocnv/segmentation_sequenza.R
@@ -0,0 +1,69 @@
+options(show.error.messages = F, error = function() {
+        cat(geterrmessage(), file = stderr()); q("no", 1, F) })
+
+# load packages that are provided in the conda env
+library(optparse)
+library(sequenza)
+library(BiocParallel)
+library(tidyverse)
+library(readr)
+
+option_list <- list(
+  make_option(
+    c("-i", "--input"),
+    default = NA,
+    type = "character",
+    help = "Path to Sequenza seqz processed segments file"
+  ),
+  make_option(
+    c("-O", "--output_dir"),
+    default = NA,
+    type = "character",
+    help = "Output directory"
+  ),
+  make_option(
+    c("-s", "--sample_name"),
+    default = NA,
+    type = "character",
+    help = "Sample name"
+  )
+)
+
+opt <- parse_args(OptionParser(option_list = option_list),
+                 args = commandArgs(trailingOnly = TRUE))
+
+data_file <- opt$input
+output_dir <- opt$output_dir
+sample_name <- opt$sample_name
+
+
+## Processing seqz files : normalisation and segmentation for chromosomes 1 to 22
+message(sprintf("\nExtraction step for %s", data_file))
+
+chrom_list <- c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9",
+               "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17",
+               "chr18", "chr19", "chr20", "chr21", "chr22")
+
+Sys.setenv(TZDIR = "/usr/share/zoneinfo/")
+Sys.setenv(TZ = "US/Eastern")
+options(tz = "US/Eastern")
+Sys.timezone(location = TRUE)
+
+
+segfile <- sequenza.extract(data_file,
+                            verbose = FALSE,
+                            chromosome.list = chrom_list)
+
+## Estimation of cellularity and ploidy
+
+segfile_cp <- sequenza.fit(segfile)
+message(sprintf("\nEstimation step for %s\n", data_file))
+
+## writing files and plots using default parameters
+message(sprintf("\nWriting files and plots for %s\n", data_file))
+
+sequenza.results(sequenza.extract = segfile,
+                 cp.table = segfile_cp,
+                 sample.id = sample_name,
+                 out.dir = output_dir)
+message(sprintf("\nOutput written to %s\n", output_dir))
