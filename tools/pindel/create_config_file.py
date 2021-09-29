9bae2cc35b71dabcb73cd586eb1bdc458132548c
Christophe Antoniewski <drosofff@users.noreply.github.com>
2021-09-29 23:13:15 +0200

    get back the support for pindel developped by Portia (#546)
    * get back the support for pindel developped by Portia
    
    * Update create_config_file.py
    
    * Update pindelwrapper.xml

---
+++ tools/pindel/create_config_file.py
@@ -0,0 +1,27 @@
+import argparse
+
+
+description = ("This script will create a configuration file for samples \
+                to be run in Pindel")
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input_file", nargs="*",
+                    help="One or more alignment files")
+parser.add_argument("--insert_size", nargs="+",
+                    help="Expected Insert size")
+parser.add_argument("--sample_label", nargs="+",
+                    help="Sample label")
+parser.add_argument("--output_config_file",
+                    help="Output config file")
+args = parser.parse_args()
+
+template = "{input_file}\t{insert_size}\t{sample_label}\n"
+with open(args.output_config_file, "w") as output:
+    for input_file, insert_size, sample_label in zip(args.input_file,
+                                                     args.insert_size,
+                                                     args.sample_label):
+        config_line = template.format(input_file=input_file,
+                                      insert_size=insert_size,
+                                      sample_label=sample_label)
+        output.write(config_line)
