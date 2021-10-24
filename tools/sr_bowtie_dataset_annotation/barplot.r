8cba23195b9f9fcb7b39ff1760f83b1db4d5c51d
drosofff <drosofff@gmail.com>
2021-10-25 01:10:14 +0200

    more r linting

--- tools/sr_bowtie_dataset_annotation/barplot.r
+++ tools/sr_bowtie_dataset_annotation/barplot.r
@@ -6,9 +6,10 @@ if (length(commandArgs(TRUE)) == 0) {
 
 # load packages that are provided in the conda env
 options(show.error.messages = F,
-       error = function() {cat(geterrmessage(), file = stderr())
+       error = function() {
+           cat(geterrmessage(), file = stderr())
                            q("no", 1, F)
-               }
+       }
 )
 loc <- Sys.setlocale("LC_MESSAGES", "en_US.UTF-8")
 warnings()
@@ -22,13 +23,13 @@ option_list <- list(
   make_option(
     c("-i", "--input"),
     default = NA,
-    type = 'character',
+    type = "character",
     help = "Input file that contains count data (no header)"
   ),
   make_option(
     c("-o", "--barplot"),
     default = NA,
-    type = 'character',
+    type = "character",
     help = "PDF output file"
   )
 )
@@ -36,26 +37,24 @@ option_list <- list(
 opt <- parse_args(OptionParser(option_list = option_list),
                  args = commandArgs(trailingOnly = TRUE))
 
-
-## 
 annotations <- read.delim(opt$input, header = F)
 colnames(annotations) <- c("sample", "class", "percent_of_reads", "total")
-annotations$percent <- round(annotations$percent_of_reads/annotations$total * 100,
+annotations$percent <- round(annotations$percent_of_reads / annotations$total * 100,
                             digits = 2)
 # ggplot2 plotting
 
 # Define the number of colors you want
-sasha_trubetskoy_palette <- c('#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231',
-                              '#911eb4', '#46f0f0', '#f032e6', '#bcf60c',
-                              '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000',
-                              '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080')
-nb.cols <- 19  # 10 with colorRampPalette
+sasha_trubetskoy_palette <- c("#e6194b", "#3cb44b", "#ffe119", "#4363d8", "#f58231",
+                              "#911eb4", "#46f0f0", "#f032e6", "#bcf60c",
+                              "#008080", "#e6beff", "#9a6324", "#fffac8", "#800000",
+                              "#aaffc3", "#808000", "#ffd8b1", "#000075", "#808080")
+nb.cols <- 19
 mycolors <- sasha_trubetskoy_palette[1:nb.cols]
 
-ggtitle('Class proportions') 
+ggtitle("Class proportions")
 ggplot(annotations, aes(x = total / 2, y = percent_of_reads, fill = class, width = total)) +
-       geom_bar(position = "fill", stat = "identity") + 
-       facet_wrap(~sample, ncol = 3 ) +
+       geom_bar(position = "fill", stat = "identity") +
+       facet_wrap(~sample, ncol = 3) +
        geom_label_repel(aes(label = percent), position = position_fill(vjust = 0.5),
                         size = 2, show.legend = F) +
        coord_polar(theta = "y") +
@@ -65,7 +64,7 @@ ggplot(annotations, aes(x = total / 2, y = percent_of_reads, fill = class, width
              axis.ticks = element_blank(),
              panel.grid  = element_blank(),
              axis.title.y = element_blank(),
-             legend.position="bottom") +
+             legend.position = "bottom") +
        geom_text(aes(x = total / 2, y = .5, label = paste(round(total / 1000000, digits = 3),
                      "M"),
                  vjust = 4, hjust = -1), size = 2)
