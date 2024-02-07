ba443cf3cb1a4da154b9977dec5c25128b7a0159
drosofff <drosofff@gmail.com>
2024-02-07 18:42:32 +0100

    Fix R code syntax

--- tools/ez_histograms/ez_histograms.R
+++ tools/ez_histograms/ez_histograms.R
@@ -5,6 +5,16 @@ library(scales)
 library(vtable)
 library(optparse)
 
+options(show.error.messages = FALSE,
+  error = function() {
+    cat(geterrmessage(), file = stderr())
+    q("no", 1, FALSE)
+  }
+)
+
+loc <- Sys.setlocale("LC_MESSAGES", "en_US.UTF-8")
+warnings()
+
 option_list <- list(
   make_option(
     c("-f", "--file"),
@@ -47,67 +57,68 @@ option_list <- list(
 opt <- parse_args(OptionParser(option_list = option_list),
                   args = commandArgs(trailingOnly = TRUE))
 
-plot_histograms <- function(mdata, profile="count", xscale="cartesian", yscale="cartesian", bins = 30){
+plot_histograms <- function(mdata, profile = "count", xscale = "cartesian", yscale = "cartesian", bins = 30) {
   if (profile == "count") {
     # count histogram
-    p <- ggplot(mdata, aes(x= value, fill=variable, color=variable, y = after_stat(count)), show.legend = FALSE) +
-      geom_histogram(bins = bins) + theme(legend.position="none")
+    p <- ggplot(mdata, aes(x = value, fill = variable, color = variable, y = after_stat(count)), show.legend = FALSE) +
+      geom_histogram(bins = bins) + theme(legend.position = "none")
     if (xscale == "cartesian") {
       if (yscale == "log2") {
-          p <- p + scale_y_continuous(trans = 'log2', labels = trans_format("log2", math_format(2^.x)))
+        p <- p + scale_y_continuous(trans = "log2", labels = trans_format("log2", math_format(2^.x)))
       } else {
-          if (yscale == "log10") {
-            p <- p + scale_y_continuous(trans = 'log10', labels = trans_format("log10", math_format(10^.x)))
-          }
+        if (yscale == "log10") {
+          p <- p + scale_y_continuous(trans = "log10", labels = trans_format("log10", math_format(10^.x)))
+        }
       }
     }
     if (xscale == "log2") {
-      p <- p + scale_x_continuous(trans = 'log2', labels = trans_format("log2", math_format(2^.x)))
+      p <- p + scale_x_continuous(trans = "log2", labels = trans_format("log2", math_format(2^.x)))
       if (yscale == "log2") {
-          p <- p + scale_y_continuous(trans = 'log2', labels = trans_format("log2", math_format(2^.x)))
-        } else {
-          if (yscale == "log10") {
-            p <- p + scale_y_continuous(trans = 'log10', labels = trans_format("log10", math_format(10^.x)))
-          }
+        p <- p + scale_y_continuous(trans = "log2", labels = trans_format("log2", math_format(2^.x)))
+      } else {
+        if (yscale == "log10") {
+            p <- p + scale_y_continuous(trans = "log10", labels = trans_format("log10", math_format(10^.x)))
         }
+      }
     }
     if (xscale == "log10") {
-      p <- p + scale_x_continuous(trans = 'log10', labels = trans_format("log10", math_format(10^.x)))
+      p <- p + scale_x_continuous(trans = "log10", labels = trans_format("log10", math_format(10^.x)))
       if (yscale == "log2") {
-          p <- p + scale_y_continuous(trans = 'log2', labels = trans_format("log2", math_format(2^.x)))
-        } else {
-          if (yscale == "log10") {
-          p <- p + scale_y_continuous(trans = 'log10', labels = trans_format("log10", math_format(10^.x)))
-          }
+        p <- p + scale_y_continuous(trans = "log2", labels = trans_format("log2", math_format(2^.x)))
+      } else {
+        if (yscale == "log10") {
+          p <- p + scale_y_continuous(trans = "log10", labels = trans_format("log10", math_format(10^.x)))
         }
+      }
     }
   }
 
   if (profile == "density") {
     # density histogram
-    p <- ggplot(mdata, aes(x= value, fill=variable, color=variable)) +
-      geom_density() + theme(legend.position="none")
+    p <- ggplot(mdata, aes(x = value, fill = variable, color = variable)) +
+      geom_density() + theme(legend.position = "none")
     if (xscale == "log2") {
-      p <- p + scale_x_continuous(trans = 'log2', labels = trans_format("log2", math_format(2^.x)))
+      p <- p + scale_x_continuous(trans = "log2", labels = trans_format("log2", math_format(2^.x)))
     }
     if (xscale == "log10") {
-      p <- p + scale_x_continuous(trans = 'log10', labels = trans_format("log10", math_format(10^.x)))
+      p <- p + scale_x_continuous(trans = "log10", labels = trans_format("log10", math_format(10^.x)))
     }
   }
   return(p)
-}  
-  
+}
+
 test_header <- function(file) {
   data <- read.delim(file = file, header = FALSE, row.names = 1, nrows = 2)
-  if(all(is.na(as.numeric(data[1, 1:ncol(data)])))) {
-    return(TRUE)}  else {
+  if (all(is.na(as.numeric(data[1, 1:seq_len(ncol(data))])))) {
+    return(TRUE)
+    } else {
       return(FALSE)
     }
 }
 
 test_rownames <- function(file) {
   data <- read.delim(file = file, header = FALSE, row.names = NULL, nrows = 2)
-  if(is.na(as.numeric(data[2, 1]))){
+  if (is.na(as.numeric(data[2, 1]))){
     return(1)
   } else {
     return(NULL)
@@ -124,7 +135,7 @@ mdata <- melt(data)
 # determine optimal number of bins (Sturges’ Rule)
 bins <- ceiling(log2(nrow(data)) + 1)
 # plot
-p <- plot_histograms(mdata, profile = opt$profile, xscale = opt$xscale, bins = bins, yscale= opt$yscale)
+p <- plot_histograms(mdata, profile = opt$profile, xscale = opt$xscale, bins = bins, yscale = opt$yscale)
 
 # determine optimal width for the graph
 width <- length(data)
@@ -153,7 +164,6 @@ dev.off()
 
 # Summary statistics with vtable package
 summary_df <- sumtable(data, digits = 8, out = "return", add.median = TRUE,
-                       summ.names = c('N','Mean','Std. Dev.','Min','Pctl. 25',
-                                      'Median', 'Pctl. 75','Max'))
+                       summ.names = c("N", "Mean", "Std. Dev.", "Min", "Pctl. 25",
+                                      "Median", "Pctl. 75", "Max"))
 write.table(summary_df, file = opt$summary, sep = "\t", quote = FALSE, row.names = FALSE)
-
