743cf253c9576bf543c2e17d11d0980abb85d6a0
Christophe Antoniewski <drosofff@gmail.com>
2024-11-07 22:39:03 +0100

    reindent R code

--- tools/gsc_scran_normalize/scran-normalize.R
+++ tools/gsc_scran_normalize/scran-normalize.R
@@ -1,8 +1,9 @@
-options(show.error.messages = FALSE,
-  error = function() {
-    cat(geterrmessage(), file = stderr())
-    q("no", 1, FALSE)
-  }
+options(
+    show.error.messages = FALSE,
+    error = function() {
+        cat(geterrmessage(), file = stderr())
+        q("no", 1, FALSE)
+    }
 )
 loc <- Sys.setlocale("LC_MESSAGES", "en_US.UTF-8")
 warnings()
@@ -13,63 +14,63 @@ library(dynamicTreeCut)
 
 # Arguments
 option_list <- list(
-  make_option(
-    c("-d", "--data"),
-    default = NA,
-    type = "character",
-    help = "Input file that contains count values to transform"
-  ),
-  make_option(
-    "--cluster",
-    default = FALSE,
-    action = "store_true",
-    type = "logical",
-    help = "Whether to calculate the size factor per cluster or on all cell"
-  ),
-  make_option(
-    c("-m", "--method"),
-    default = "hclust",
-    type = "character",
-    help = "The clustering method to use for grouping cells into cluster : hclust or igraph [default : '%default' ]"
-  ),
-  make_option(
-    "--size",
-    default = 100,
-    type = "integer",
-    help = "Minimal number of cells in each cluster : hclust or igraph [default : '%default' ]"
-  ),
-  make_option(
-    c("-o", "--out"),
-    default = "res.tab",
-    type = "character",
-    help = "Output name [default : '%default' ]"
-  )
+    make_option(
+        c("-d", "--data"),
+        default = NA,
+        type = "character",
+        help = "Input file that contains count values to transform"
+    ),
+    make_option(
+        "--cluster",
+        default = FALSE,
+        action = "store_true",
+        type = "logical",
+        help = "Whether to calculate the size factor per cluster or on all cell"
+    ),
+    make_option(
+        c("-m", "--method"),
+        default = "hclust",
+        type = "character",
+        help = "The clustering method to use for grouping cells into cluster : hclust or igraph [default : '%default' ]"
+    ),
+    make_option(
+        "--size",
+        default = 100,
+        type = "integer",
+        help = "Minimal number of cells in each cluster : hclust or igraph [default : '%default' ]"
+    ),
+    make_option(
+        c("-o", "--out"),
+        default = "res.tab",
+        type = "character",
+        help = "Output name [default : '%default' ]"
+    )
 )
 
 opt <- parse_args(OptionParser(option_list = option_list),
-                  args = commandArgs(trailingOnly = TRUE))
+    args = commandArgs(trailingOnly = TRUE)
+)
 
 
 data <- read.table(
-  opt$data,
-  check.names = FALSE,
-  header = TRUE,
-  row.names = 1,
-  sep = "\t"
+    opt$data,
+    check.names = FALSE,
+    header = TRUE,
+    row.names = 1,
+    sep = "\t"
 )
 
 ## Import data as a SingleCellExperiment object
 sce <- SingleCellExperiment(list(counts = as.matrix(data)))
 
 if (opt$cluster) {
-  clusters <- quickCluster(sce, min.size = opt$size, method = opt$method)
+    clusters <- quickCluster(sce, min.size = opt$size, method = opt$method)
 
-  ## Compute sum factors
-  sce <- computeSumFactors(sce, cluster = clusters)
+    ## Compute sum factors
+    sce <- computeSumFactors(sce, cluster = clusters)
 } else {
-
-  ## Compute sum factors
-  sce <- computeSumFactors(sce)
+    ## Compute sum factors
+    sce <- computeSumFactors(sce)
 }
 
 sce <- logNormCounts(sce)
@@ -78,10 +79,10 @@ logcounts <- data.frame(genes = rownames(sce), round(logcounts(sce), digits = 5)
 
 
 write.table(
-  logcounts,
-  opt$out,
-  col.names = TRUE,
-  row.names = FALSE,
-  quote = FALSE,
-  sep = "\t"
+    logcounts,
+    opt$out,
+    col.names = TRUE,
+    row.names = FALSE,
+    quote = FALSE,
+    sep = "\t"
 )
